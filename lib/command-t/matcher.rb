e6c5c18b7f88c22c88b25e698736ea37f98e7411
Wincent Colaiuta <win@wincent.com>
2010-02-26 20:52:40 +0100

    Move Matcher into subdirectory
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ lib/command-t/matcher.rb
@@ -0,0 +1,28 @@
+module CommandT
+  class Matcher
+    attr_accessor :paths
+
+    # Turn a search string like:
+    #   "foo"
+    # Into a regular expression like:
+    #   /.*(f).*(o).*(o).*/i
+    def self.regexp_for str
+      raise ArgumentError.new('nil str') if str.nil?
+      return // if str.empty?
+      chars = str.chars.map { |c| '(' + Regexp.escape(c) + ')' }
+      glob = '.*'
+      Regexp.new(glob + (chars.join glob) + glob, Regexp::IGNORECASE)
+    end
+
+    def initialize *paths
+      @paths = paths
+    end
+
+    def matches_for str
+      regexp = self.class.regexp_for str
+      @paths.select do |path|
+        !path.match(regexp).nil?
+      end
+    end
+  end # class Matcher
+end # module CommandT
