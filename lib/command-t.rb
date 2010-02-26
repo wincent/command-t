712da39c906928d8dc7be7874a1c1035149ce8d9
Wincent Colaiuta <win@wincent.com>
2010-02-26 20:47:42 +0100

    Move matching functionality into "Matcher" class
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- lib/command-t.rb
+++ lib/command-t.rb
@@ -1,22 +1,28 @@
 class CommandT
-  # Turn a search string like:
-  #   "foo"
-  # Into a regular expression like:
-  #   /.*(f).*(o).*(o).*/i
-  def self.regexp_for str
-    chars = str.chars.map { |c| '(' + Regexp.escape(c) + ')' }
-    glob = '.*'
-    Regexp.new(glob + (chars.join glob) + glob, Regexp::IGNORECASE)
-  end
+  class Matcher
+    attr_accessor :paths
 
-  def initialize *paths
-    @paths = paths
-  end
-
-  def matches_for str
-    regexp = self.class.regexp_for str
-    @paths.select do |path|
-      !path.match(regexp).nil?
+    # Turn a search string like:
+    #   "foo"
+    # Into a regular expression like:
+    #   /.*(f).*(o).*(o).*/i
+    def self.regexp_for str
+      raise ArgumentError.new('nil str') if str.nil?
+      return // if str.empty?
+      chars = str.chars.map { |c| '(' + Regexp.escape(c) + ')' }
+      glob = '.*'
+      Regexp.new(glob + (chars.join glob) + glob, Regexp::IGNORECASE)
     end
-  end
+
+    def initialize *paths
+      @paths = paths
+    end
+
+    def matches_for str
+      regexp = self.class.regexp_for str
+      @paths.select do |path|
+        !path.match(regexp).nil?
+      end
+    end
+  end # class Matcher
 end # class CommandT
--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,25 +1,39 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
-describe CommandT do
+describe CommandT::Matcher do
   describe 'regexp_for method' do
     it 'should insert globs before and after every character' do
-      CommandT.regexp_for('foo').should == /.*(f).*(o).*(o).*/i
+      CommandT::Matcher.regexp_for('foo').should == /.*(f).*(o).*(o).*/i
+    end
+
+    it 'should return empty regexp for empty search string' do
+      CommandT::Matcher.regexp_for('').should == // # match all files
+    end
+
+    it 'should raise an ArgumentError if passed nil' do
+      lambda { CommandT::Matcher.regexp_for(nil) }.
+        should raise_error(ArgumentError)
+    end
+
+    it 'should escape characters which have special meaning' do
+      CommandT::Matcher.regexp_for('.rb').should == /.*(\.).*(r).*(b).*/i
     end
   end
+
   describe 'matches_for method' do
     it 'should return empty array when source array empty' do
-      @no_paths = CommandT.new
+      @no_paths = CommandT::Matcher.new
       @no_paths.matches_for('foo').should == []
       @no_paths.matches_for('').should == []
     end
 
     it 'should return empty array when no matches' do
-      @no_matches = CommandT.new './foo', './bar'
+      @no_matches = CommandT::Matcher.new './foo', './bar'
       @no_matches.matches_for('xyz').should == []
     end
 
     it 'should return matching paths' do
-      @foo_paths = CommandT.new './foo/bar', './foo/baz', './bing'
+      @foo_paths = CommandT::Matcher.new './foo/bar', './foo/baz', './bing'
       @foo_paths.matches_for('z').should == ['./foo/baz']
       @foo_paths.matches_for('bg').should == ['./bing']
     end
