35ff0f331a1b698dea05ce090e26cd98871d7049
Wincent Colaiuta <win@wincent.com>
2010-02-26 21:22:47 +0100

    Optionally override Scanner base path
    This is principally for testing purposes, because in actual use the
    intended purpose of the add-on is to always operate in the current
    working directory.
    
    In testing, however, it is useful to provide a helper method (in the
    spec_helper.rb file) to generate a new Scanner object configured to
    search the "fixtures" directory, without having to explicitly change
    directories within the specs themselves.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- lib/command-t/scanner.rb
+++ lib/command-t/scanner.rb
@@ -4,12 +4,26 @@ module CommandT
   # different scanning methods (calling "find", staying within Ruby, using a
   # C extension that uses the FSEvents API etc).
   class Scanner
+
+    # Optionally accept a specific path to use (principally for testing).
+    def initialize path = Dir.pwd
+      @path = path
+    end
+
     def flush
       @paths = nil
     end
 
     def paths
-      @paths ||= `find . -type f 2> /dev/null`.split("\n")
+      return @paths unless @paths.nil?
+      begin
+        pwd = Dir.pwd
+        Dir.chdir @path
+        @paths = `find . -type f 2> /dev/null`.split("\n")
+      ensure
+        Dir.chdir pwd
+      end
+      @paths
     end
   end
 end # module CommandT
