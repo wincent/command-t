a06c196a6d27ccd3d64478b256001028ee1197f1
Wincent Colaiuta <win@wincent.com>
2010-02-26 20:54:14 +0100

    Move Matcher spec into subdirectory also
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/command-t/matcher_spec.rb
+++ spec/command-t/matcher_spec.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), 'spec_helper')
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe CommandT::Matcher do
   describe 'regexp_for method' do
