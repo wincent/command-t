2dada1081d71eddb881d7d0fbd1a7088569a77d2
Wincent Colaiuta <win@wincent.com>
2010-02-27 15:15:39 +0100

    Refactor match specs
    Add a helper method which makes these tests more concise and will enable
    other future tests.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/command-t/match_spec.rb
+++ spec/command-t/match_spec.rb
@@ -1,17 +1,25 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe CommandT::Match do
+  def matcher_for path, pattern
+    # reuse Matcher code rather than replicate it
+    regexp = CommandT::Matcher.regexp_for pattern
+    CommandT::Match.new(regexp.match(path))
+  end
+
   describe 'score method' do
     it 'should assign non-matches a score of zero' do
-      CommandT::Match.new(nil).score.should == 0.0
+      matcher_for('./foo', 'bar').score.should == 0.0
+    end
+
+    it 'should assign perfect matches a score of one' do
+      matcher_for('./foo', './foo').score.should == 1.0
     end
   end
 
   describe 'to_s method' do
     it 'should return the entire matched string' do
-      regexp = /foo\d+/
-      match = regexp.match 'foo123'
-      CommandT::Match.new(match).to_s.should == 'foo123'
+      matcher_for('abc', 'abc').to_s.should == 'abc'
     end
   end
 end
