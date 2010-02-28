b701ad420fd641c466b96b62a81d670d3c58cab4
Wincent Colaiuta <win@wincent.com>
2010-02-28 22:28:43 +0100

    Add path= method to CommandT::Base
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- lib/command-t/base.rb
+++ lib/command-t/base.rb
@@ -5,8 +5,15 @@ module CommandT
       @matcher = Matcher.new *@scanner.paths
     end
 
+    # TODO: add :limit => 50
+    # when no search string ret in alph order
+    # when search string ret in score order
     def matches_for str
       @matcher.matches_for str
+    end
+
+    def path= path
+      @scanner.path = path
     end
   end # class Base
 end # CommandT
