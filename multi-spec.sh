06d802d5d2010a62a5cd26ec28cf26ddd84aa7ff
Wincent Colaiuta <win@wincent.com>
2010-03-24 21:25:40 +0100

    Add multispec.sh
    This script builds the extension under different versions of Ruby and
    then runs the spec suite.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ multi-spec.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -e
+
+function build_quietly()
+{
+  cd ruby/command-t
+  ruby extconf.rb > /dev/null
+  make clean > /dev/null
+  make > /dev/null
+  cd -
+}
+
+OLD_PATH=$PATH
+for RUBY_VERSION in $(ls ~/.multiruby/install); do
+  echo "$RUBY_VERSION: building"
+  export PATH=~/.multiruby/install/$RUBY_VERSION/bin:$OLD_PATH
+  build_quietly
+  echo "$RUBY_VERSION: running spec suite"
+  spec spec
+  echo "$RUBY_VERSION: finished"
+done
+
+# put things back the way we found them
+export PATH=$OLD_PATH
+echo "Restoring: $(ruby -v)"
+build_quietly
