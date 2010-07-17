6d28321e80addd3e812bd927efb3ecc628f19242
Wincent Colaiuta <win@wincent.com>
2010-07-17 12:35:26 +0200

    Move top-level VIM additions into CommandT namespace
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- ruby/vim.rb
+++ ruby/vim.rb
@@ -24,18 +24,20 @@
 require 'vim/screen'
 require 'vim/window'
 
-module VIM
-  def self.has_syntax?
-    ::VIM::evaluate('has("syntax")').to_i != 0
-  end
+module CommandT
+  module VIM
+    def self.has_syntax?
+      ::VIM::evaluate('has("syntax")').to_i != 0
+    end
 
-  def self.pwd
-    ::VIM::evaluate 'getcwd()'
-  end
+    def self.pwd
+      ::VIM::evaluate 'getcwd()'
+    end
 
-  # Escape a string for safe inclusion in a Vim single-quoted string
-  # (single quotes escaped by doubling, everything else is literal)
-  def self.escape_for_single_quotes str
-    str.gsub "'", "''"
-  end
-end # module VIM
+    # Escape a string for safe inclusion in a Vim single-quoted string
+    # (single quotes escaped by doubling, everything else is literal)
+    def self.escape_for_single_quotes str
+      str.gsub "'", "''"
+    end
+  end # module VIM
+end # module CommandT
