dde364beb08a1b79f9cd6b407aee700e221b8928
Wincent Colaiuta <win@wincent.com>
2010-07-17 13:06:50 +0200

    Use Bundler bin stubs from multi-spec.sh
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- multi-spec.sh
+++ multi-spec.sh
@@ -15,7 +15,7 @@ for RUBY_VERSION in $(ls ~/.multiruby/install); do
   export PATH=~/.multiruby/install/$RUBY_VERSION/bin:$OLD_PATH
   build_quietly
   echo "$RUBY_VERSION: running spec suite"
-  rspec spec
+  bin/rspec spec
   echo "$RUBY_VERSION: finished"
 done
 
