fe308e559d3e50887e2b57115c66bd1cea9fed31
Wincent Colaiuta <win@wincent.com>
2010-07-18 20:50:21 +0200

    Remove method used in only one place
    Since we had to make this a class method in order to leave the Vim
    module untouched, there is less reason to have this as a separate,
    self-documenting method now.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- ruby/command-t/vim/window.rb
+++ ruby/command-t/vim/window.rb
@@ -25,17 +25,13 @@ module CommandT
   module VIM
     class Window
       def self.select window
-        return true if selected?(window)
+        return true if $curwin == window
         initial = $curwin
         while true do
           ::VIM::command 'wincmd w'           # cycle through windows
           return true if $curwin == self      # have selected desired window
           return false if $curwin == initial  # have already looped through all
         end
-      end
-
-      def self.selected? window
-        $curwin == window
       end
     end # class Window
   end # module VIM
