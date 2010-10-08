d79616985fc2ade5d789c97a8e43176b730e5b5a
Wincent Colaiuta <win@wincent.com>
2010-10-08 18:38:03 +0200

    Require spec helper using relative path
    RSpec adds the spec directory to the load path, so there's no need to
    specify an absolute path.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/command-t/match_spec.rb
+++ spec/command-t/match_spec.rb
@@ -21,7 +21,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-require File.expand_path('../spec_helper', File.dirname(__FILE__))
+require 'spec_helper'
 require 'command-t/ext'
 
 describe CommandT::Match do
