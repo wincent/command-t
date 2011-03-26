da3c82128df7ba1475f49c9b50189e2d5211c397
Wincent Colaiuta <win@wincent.com>
2011-03-26 04:28:24 -0700

    Add specs for BufferScanner
    This are rudimentary specs which contain some less than ideal mocking.
    Note how we're returning the same path multiple times because we can't
    use RR's block syntax for return values here without causing the Ruby
    parser to choke.
    
    Nevertheless, the specs are still useful, enabling me to find the
    off-by-one error corrected in the last commit.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/command-t/scanner/buffer_scanner_spec.rb
@@ -0,0 +1,52 @@
+# Copyright 2010-2011 Wincent Colaiuta. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+require 'spec_helper'
+require 'ostruct'
+require 'command-t/scanner/buffer_scanner'
+
+module VIM
+  class Buffer; end
+end
+
+describe CommandT::BufferScanner do
+  def buffer name
+    b = OpenStruct.new
+    b.name = name
+    b
+  end
+
+  before do
+    @buffers = Array.new(5) { buffer 'x' }
+    @scanner = CommandT::BufferScanner.new
+    stub(@scanner).relative_path_under_working_directory(anything) { |arg| arg }
+    stub(::VIM::Buffer).count { 5 }
+    stub(::VIM::Buffer)[anything].times(5).returns(buffer 'x')
+  end
+
+  describe 'paths method' do
+    it 'returns a list of regular files' do
+      @scanner.paths.should =~ Array.new(5) { 'x' }
+    end
+  end
+end
