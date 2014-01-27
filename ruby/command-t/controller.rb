wincent/command-t

commit ff72bd9bdffaf84247288a0d3ee7b0054db7d3a7

Debounce key-presses to improve responsiveness
This idea is based on a patch from Yiding Jia, with some additional
logic added to ensure that potentially expensive matching only occurs
when really needed (ie. we don't re-match just because you hit a cursor
up or down key).

Not sure what the sweet spot for the debounce interval is, so starting
with 50ms, and making it configurable. I suspect that on really large
hierarchies you might want this to be higher (say, 100, 150 or even
200ms) in order to reduce the likelihood of unwanted match computations
kicking off just because you happened to slow a little in typing.

Note the use of augroups here to ensure that we don't keep adding more
and more autocmds to the buffer (depending on the platform, the buffer
will be cleaned up with bwipeout! or bunload!; in practice the same
buffer is usually used over and over again, so we wind up with a single
autocmd assigned to a buffer designated as `<buffer=2>` or similar).

Signed-off-by: Wincent Colaiuta <win@wincent.com>

ruby/command-t/controller.rb
@@ -1,4 +1,4 @@
-# Copyright 2010-2013 Wincent Colaiuta. All rights reserved.
+# Copyright 2010-2014 Wincent Colaiuta. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -95,7 +95,7 @@ module CommandT
     def refresh
       return unless @active_finder && @active_finder.respond_to?(:flush)
       @active_finder.flush
-      list_matches
+      list_matches!
     end
 
     def flush
@@ -109,7 +109,7 @@ module CommandT
       key = ::VIM::evaluate('a:arg').to_i.chr
       if @focus == @prompt
         @prompt.add! key
-        list_matches
+        @needs_update = true
       else
         @match_window.find key
       end
@@ -118,14 +118,14 @@ module CommandT
     def backspace
       if @focus == @prompt
         @prompt.backspace!
-        list_matches
+        @needs_update = true
       end
     end
 
     def delete
       if @focus == @prompt
         @prompt.delete!
-        list_matches
+        @needs_update.true
       end
     end
 
@@ -155,7 +155,7 @@ module CommandT
 
     def clear
       @prompt.clear!
-      list_matches
+      list_matches!
     end
 
     def cursor_left
@@ -182,7 +182,24 @@ module CommandT
       @match_window.unload
     end
 
+    def list_matches(options = {})
+      return unless @needs_update || options[:force]
+
+      @matches = @active_finder.sorted_matches_for(
+        @prompt.abbrev,
+        :limit   => match_limit,
+        :threads => CommandT::Util.processor_count
+      )
+      @match_window.matches = @matches
+
+      @needs_update = false
+    end
+
   private
+
+    def list_matches!
+      list_matches(:force => true)
+    end
 
     def show
       @initial_window   = $curwin
@@ -196,6 +213,7 @@ module CommandT
       @focus            = @prompt
       @prompt.focus
       register_for_key_presses
+      set_up_autocmds
       clear # clears prompt and lists matches
     end
 
@@ -333,6 +351,16 @@ module CommandT
       end
     end
 
+    def set_up_autocmds
+      debounce_interval = get_number('g:CommandTInputDebounce') || 50
+
+      ::VIM::command 'augroup Command-T'
+      ::VIM::command 'au!'
+      ::VIM::command 'autocmd CursorHold <buffer> :call CommandTListMatches()'
+      ::VIM::command "setlocal updatetime=#{debounce_interval}"
+      ::VIM::command 'augroup END'
+    end
+
     # Returns the desired maximum number of matches, based on available
     # vertical space and the g:CommandTMaxHeight option.
     def match_limit
@@ -340,15 +368,6 @@ module CommandT
       limit = 1 if limit < 0
       limit = [limit, max_height].min if max_height > 0
       limit
-    end
-
-    def list_matches
-      @matches = @active_finder.sorted_matches_for(
-        @prompt.abbrev,
-        :limit   => match_limit,
-        :threads => CommandT::Util.processor_count
-      )
-      @match_window.matches = @matches
     end
 
     def buffer_finder

LICENSE
@@ -1,4 +1,4 @@
-Copyright 2010-2012 Wincent Colaiuta. All rights reserved.
+Copyright 2010-2014 Wincent Colaiuta. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:

plugin/command-t.vim
@@ -1,5 +1,5 @@
 " command-t.vim
-" Copyright 2010-2013 Wincent Colaiuta. All rights reserved.
+" Copyright 2010-2014 Wincent Colaiuta. All rights reserved.
 "
 " Redistribution and use in source and binary forms, with or without
 " modification, are permitted provided that the following conditions are met:
@@ -91,6 +91,10 @@ endfunction
 if !has('ruby')
   finish
 endif
+
+function CommandTListMatches()
+  ruby $command_t.list_matches
+endfunction
 
 function CommandTHandleKey(arg)
   ruby $command_t.handle_key

commit e82448d0acb6eb9f06f99d388cd95be3c49d2e16

Fix typo in Controller#delete method
This typo was originally introduced in commit ff72bd9bdffaf84247288a0.

Signed-off-by: Wincent Colaiuta <win@wincent.com>

ruby/command-t/controller.rb
@@ -125,7 +125,7 @@ module CommandT
     def delete
       if @focus == @prompt
         @prompt.delete!
-        @needs_update.true
+        @needs_update = true
       end
     end
 