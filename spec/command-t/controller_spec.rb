f9ea3dbcc5d684b91e7330a1117b9127317e7441
Wincent Colaiuta <win@wincent.com>
2014-03-04 17:54:58 -0800

    Fix controller spec failures
    With "judicious" stubbing. The amount of stubbing in this spec is an
    immense smell, but pursuing the quick fix in the name of getting the
    suite green again rather than engaging in a deeper reworking for now.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/command-t/controller_spec.rb
+++ spec/command-t/controller_spec.rb
@@ -1,4 +1,4 @@
-# Copyright 2010-2013 Wincent Colaiuta. All rights reserved.
+# Copyright 2010-2014 Wincent Colaiuta. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -91,6 +91,9 @@ describe CommandT::Controller do
     stub($curbuf).number.returns('0')
     stub(::VIM).command(/noremap/)
     stub(::VIM).command('silent b 0')
+    stub(::VIM).command(/augroup/)
+    stub(::VIM).command('au!')
+    stub(::VIM).command(/autocmd/)
     stub(::VIM).evaluate(/exists\(.+\)/).returns('0')
     stub(::VIM).evaluate('getcwd()').returns(working_directory)
     stub(::VIM).evaluate('&buflisted').returns('1')
