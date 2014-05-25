wincent/command-t

commit 301fd661831ca7a024007a603658fe38a817f8ec

Fix off-by-one error in memoization indexing
I believe this was causing more cache lookups to miss than should have
been the case. At least with the meagre test data I was using, the
quality of the results wasn't being obviously affected.

With this change, the "pathological" test case gets much, much faster,
while the others remain more or less the same.

Old:

                          user     system      total        real
  pathological        2.420000   0.000000   2.420000 (  2.450693)
  command-t           3.260000   0.030000   3.290000 (  3.299227)
  chromium (subset)   2.660000   0.020000   2.680000 (  2.672489)
  chromium (whole)    7.750000   0.050000   7.800000 (  7.827607)

New:

                          user     system      total        real
  pathological        0.000000   0.000000   0.000000 (  0.004769)
  command-t           3.330000   0.030000   3.360000 (  3.368512)
  chromium (subset)   2.570000   0.020000   2.590000 (  2.591855)
  chromium (whole)    7.080000   0.040000   7.120000 (  7.119191)

The improvement is so marked that I am almost suspicious of it, but I
added logging of -- lengths and indices, hits and misses, loads and
stores -- and ran it over a simple test case and it looks like the right
values are being loaded/stored in the cache.

Signed-off-by: Wincent Colaiuta <win@wincent.com>

ruby/command-t/match.c
@@ -129,7 +129,7 @@ double recursive_match(matchinfo_t *m,    // sharable meta-data
                 }
 
                 score += score_for_char;
-                last_idx = haystack_idx++;
+                last_idx = haystack_idx + 1;
                 break;
             }
         }

commit 4ed8de465f151b95ec70a8b07bfec3129f486747

Fix off-by-one error in last_idx management
This is a relatively benign error; causing our last_idx calculations to
be slightly more favorable than they should have been, leading to
slightly inflated match scores. I think this was likely harmless,
because all matches would have benefited from this bias.

This is how the code used to look before the ill-fated 301fd661831,
which introduced a nasty matching bug. I fixed this in 5621c4e2185, but
introduced this other error in my haste; I probably should have realized
it was wrong by comparison with the pre-301fd661831 state.

Signed-off-by: Greg Hurrell <greg@hurrell.net>

ruby/command-t/match.c
@@ -125,7 +125,7 @@ double recursive_match(matchinfo_t *m,    // sharable meta-data
                 }
 
                 score += score_for_char;
-                last_idx = ++haystack_idx;
+                last_idx = haystack_idx++;
                 break;
             }
         }