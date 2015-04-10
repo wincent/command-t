wincent/command-t

commit 542ccd4ab1c57cc73d484f3be708c3aa3bfdff75

Simplify inheritance in FileScanner subclasses
The design of the FileScanner hierarchy has bugged me for a long time.
This commit cleans it up substantially, although there is still more
that could be done.

The re-use wasn't flowing in the right direction here, which showed up
as un-DRY-ness. Specifically, the superclass implemented caching in the
`#paths` method and the subclasses tried to leverage that by calling
`super`, but in some places the logic was duplicated (eg. wildignore
handling, cache updating and other management via explicit hash key
manipulation and calls to `#prepare_paths`).

This refactoring centralizes those concerns in the superclass, where
they belong. We delegate to the subclasses only for the cache-miss edge
case (edge case because it generally happen once only). This brings a
few benefits:

- no more cache logic (explicit key use) in subclasses
- no more cache management via `#prepare_paths` in subclasses
- no more wildignore management in subclasses
- less explicit exception-handling in subclasses
- less nesting in subclasses

The major thing that still smells is a mix of explicit param passing
and implicit communication via instance variables. This can be cleaned
up in a separate commit.

ruby/command-t/scanner/file_scanner/watchman_file_scanner.rb
@@ -20,43 +20,38 @@ module CommandT
       # requested path.
       class WatchmanUnavailable < RuntimeError; end
 
-      def paths
-        @paths[@path] ||= begin
-          prepare_paths
-          sockname = Watchman::Utils.load(
-            %x{watchman --output-encoding=bser get-sockname}
-          )['sockname']
-          raise WatchmanUnavailable unless $?.exitstatus.zero?
+      def paths!
+        sockname = Watchman::Utils.load(
+          %x{watchman --output-encoding=bser get-sockname}
+        )['sockname']
+        raise WatchmanUnavailable unless $?.exitstatus.zero?
 
-          UNIXSocket.open(sockname) do |socket|
-            root = Pathname.new(@path).realpath.to_s
-            roots = Watchman::Utils.query(['watch-list'], socket)['roots']
-            if !roots.include?(root)
-              # this path isn't being watched yet; try to set up watch
-              result = Watchman::Utils.query(['watch', root], socket)
+        UNIXSocket.open(sockname) do |socket|
+          root = Pathname.new(@path).realpath.to_s
+          roots = Watchman::Utils.query(['watch-list'], socket)['roots']
+          if !roots.include?(root)
+            # this path isn't being watched yet; try to set up watch
+            result = Watchman::Utils.query(['watch', root], socket)
 
-              # root_restrict_files setting may prevent Watchman from working
-              raise WatchmanUnavailable if result.has_key?('error')
-            end
-
-            query = ['query', root, {
-              'expression' => ['type', 'f'],
-              'fields'     => ['name'],
-            }]
-            paths = Watchman::Utils.query(query, socket)
-
-            # could return error if watch is removed
-            raise WatchmanUnavailable if paths.has_key?('error')
-
-            @paths[@path] = paths['files']
+            # root_restrict_files setting may prevent Watchman from working
+            raise WatchmanUnavailable if result.has_key?('error')
           end
-        end
 
-        @paths[@path]
-      rescue Errno::ENOENT, WatchmanUnavailable
-        # watchman executable not present, or unable to fulfil request
-        super
+          query = ['query', root, {
+            'expression' => ['type', 'f'],
+            'fields'     => ['name'],
+          }]
+          paths = Watchman::Utils.query(query, socket)
+
+          # could return error if watch is removed
+          raise WatchmanUnavailable if paths.has_key?('error')
+
+          paths['files']
+        end
       end
+    rescue Errno::ENOENT, WatchmanUnavailable
+      # watchman executable not present, or unable to fulfil request
+      super
     end # class WatchmanFileScanner
   end # class FileScanner
 end # module CommandT

ruby/command-t/scanner/file_scanner.rb
@@ -26,15 +26,11 @@ module CommandT
       @base_wild_ignore     = VIM::wild_ignore
     end
 
-    def prepare_paths
-      ensure_cache_under_limit
-      @prefix_len = @path.chomp('/').length
-    end
-
     def paths
-      @paths[@path] || begin
-        prepare_paths
-        nil
+      @paths[@path] ||= begin
+        ensure_cache_under_limit
+        @prefix_len = @path.chomp('/').length
+        set_wild_ignore { paths! }
       end
     end
 
@@ -43,6 +39,10 @@ module CommandT
     end
 
   private
+
+    def paths!
+      raise RuntimeError, 'subclass responsibility'
+    end
 
     def ensure_cache_under_limit
       # Ruby 1.8 doesn't have an ordered hash, so use a separate stack to
@@ -62,18 +62,21 @@ module CommandT
       end
     end
 
+    def has_custom_wild_ignore?
+      @wild_ignore && !@wild_ignore.empty?
+    end
+
     # Used to skip expensive calls to `expand()` when there is no applicable
     # wildignore.
     def apply_wild_ignore?
-      if @wild_ignore
-        return !@wild_ignore.empty?
-      else
-        return !@base_wild_ignore.empty?
-      end
+      has_custom_wild_ignore? || !@base_wild_ignore.empty?
     end
 
-    def set_wild_ignore(ignore)
-      ::VIM::command("set wildignore=#{ignore}") if @wild_ignore
+    def set_wild_ignore(&block)
+      ::VIM::command("set wildignore=#{@wild_ignore}") if has_custom_wild_ignore?
+      yield
+    ensure
+      ::VIM::command("set wildignore=#{@base_wild_ignore}") if has_custom_wild_ignore?
     end
   end # class FileScanner
 end # module CommandT

ruby/command-t/scanner/file_scanner/find_file_scanner.rb
@@ -12,45 +12,39 @@ module CommandT
     class FindFileScanner < FileScanner
       include VIM::PathUtilities
 
-      def paths
-        super || begin
-          set_wild_ignore(@wild_ignore)
+      def paths!
+        # temporarily set field separator to NUL byte; this setting is
+        # respected by both `readlines` and `chomp!` below, and makes it easier
+        # to parse the output of `find -print0`
+        separator = $/
+        $/ = "\x00"
 
-          # temporarily set field separator to NUL byte; this setting is
-          # respected by both `readlines` and `chomp!` below, and makes it easier
-          # to parse the output of `find -print0`
-          separator = $/
-          $/ = "\x00"
-
-          unless @scan_dot_directories
-            dot_directory_filter = [
-              '-not', '-path', "#{@path}/.*/*",           # top-level dot dir
-              '-and', '-not', '-path', "#{@path}/*/.*/*"  # lower-level dot dir
-            ]
-          end
-
-          Open3.popen3(*([
-            'find', '-L',                 # follow symlinks
-            @path,                        # anchor search here
-            '-maxdepth', @max_depth.to_s, # limit depth of DFS
-            '-type', 'f',                 # only show regular files (not dirs etc)
-            dot_directory_filter,         # possibly skip out dot directories
-            '-print0'                     # NUL-terminate results
-          ].flatten.compact)) do |stdin, stdout, stderr|
-            counter = 1
-            paths = []
-            stdout.readlines.each do |line|
-              next if path_excluded?(line.chomp!)
-              paths << line[@prefix_len + 1..-1]
-              break if (counter += 1) > @max_files
-            end
-            @paths[@path] = paths
-          end
-        ensure
-          $/ = separator
-          set_wild_ignore(@base_wild_ignore)
+        unless @scan_dot_directories
+          dot_directory_filter = [
+            '-not', '-path', "#{@path}/.*/*",           # top-level dot dir
+            '-and', '-not', '-path', "#{@path}/*/.*/*"  # lower-level dot dir
+          ]
         end
-        @paths[@path]
+
+        paths = []
+        Open3.popen3(*([
+          'find', '-L',                 # follow symlinks
+          @path,                        # anchor search here
+          '-maxdepth', @max_depth.to_s, # limit depth of DFS
+          '-type', 'f',                 # only show regular files (not dirs etc)
+          dot_directory_filter,         # possibly skip out dot directories
+          '-print0'                     # NUL-terminate results
+        ].flatten.compact)) do |stdin, stdout, stderr|
+          counter = 1
+          stdout.readlines.each do |line|
+            next if path_excluded?(line.chomp!)
+            paths << line[@prefix_len + 1..-1]
+            break if (counter += 1) > @max_files
+          end
+        end
+        paths
+      ensure
+        $/ = separator
       end
     end # class FindFileScanner
   end # class FileScanner

ruby/command-t/scanner/file_scanner/git_file_scanner.rb
@@ -7,34 +7,27 @@ module CommandT
   class FileScanner
     # Uses git ls-files to scan for files
     class GitFileScanner < FindFileScanner
-      def paths
-        @paths[@path] ||= begin
-          Dir.chdir(@path) do
-            set_wild_ignore(@wild_ignore)
-            prepare_paths
+      def paths!
+        Dir.chdir(@path) do
+          stdin, stdout, stderr = Open3.popen3(*[
+            'git',
+            'ls-files',
+            '--exclude-standard',
+            @path
+          ])
 
-            stdin, stdout, stderr = Open3.popen3(*[
-              'git',
-              'ls-files',
-              '--exclude-standard',
-              @path
-            ])
+          all_files = stdout.readlines.
+            map { |path| path.chomp }.
+            reject { |path| path_excluded?(path, 0) }.
+            take(@max_files).
+            to_a
 
-            all_files = stdout.readlines.
-              map { |path| path.chomp }.
-              reject { |path| path_excluded?(path, 0) }.
-              take(@max_files).
-              to_a
-
-            # will fall back to find if not a git repository or there's an error
-            stderr.gets ? super : all_files
-          end
-        rescue Errno::ENOENT => e
-          # git executable not present and executable
-          super
-        ensure
-          set_wild_ignore(@base_wild_ignore)
+          # will fall back to find if not a git repository or there's an error
+          stderr.gets ? super : all_files
         end
+      rescue Errno::ENOENT => e
+        # git executable not present and executable
+        super
       end
     end # class GitFileScanner
   end # class FileScanner

ruby/command-t/scanner/file_scanner/ruby_file_scanner.rb
@@ -8,30 +8,26 @@ module CommandT
   class FileScanner
     # Pure Ruby implementation of a file scanner.
     class RubyFileScanner < FileScanner
-      def paths
-        super || begin
-          @paths[@path] = []
-          @depth        = 0
-          @files        = 0
-          set_wild_ignore(@wild_ignore)
-          add_paths_for_directory @path, @paths[@path]
-        rescue FileLimitExceeded
-        ensure
-          set_wild_ignore(@base_wild_ignore)
-        end
-        @paths[@path]
+      def paths!
+        accumulator = []
+        @depth = 0
+        @files = 0
+        add_paths_for_directory(@path, accumulator)
+        accumulator
+      rescue FileLimitExceeded
+        accumulator
       end
 
     private
 
-      def looped_symlink? path
+      def looped_symlink?(path)
         if File.symlink?(path)
           target = File.expand_path(File.readlink(path), File.dirname(path))
           target.include?(@path) || @path.include?(target)
         end
       end
 
-      def add_paths_for_directory dir, accumulator
+      def add_paths_for_directory(dir, accumulator)
         Dir.foreach(dir) do |entry|
           next if ['.', '..'].include?(entry)
           path = File.join(dir, entry)
@@ -45,7 +41,7 @@ module CommandT
               next if (entry.match(/\A\./) && !@scan_dot_directories)
               next if looped_symlink?(path)
               @depth += 1
-              add_paths_for_directory path, accumulator
+              add_paths_for_directory(path, accumulator)
               @depth -= 1
             end
           end

commit 05bc4ba3b5db3a9828c74cfadc21d7f92f2b47ba

Fix incredibly clowny mis-nesting
Because we were rescuing within the class body and not the method body,
Watchman errors were bubbling up uncaught.

I think this has been broken since 542ccd4ab1c, suggesting I need a
regression test, which I will try and follow up with later.

ruby/command-t/scanner/file_scanner/watchman_file_scanner.rb
@@ -46,10 +46,10 @@ module CommandT
 
             paths['files']
           end
+        rescue Errno::ENOENT, WatchmanError
+          # watchman executable not present, or unable to fulfil request
+          super
         end
-      rescue Errno::ENOENT, WatchmanError
-        # watchman executable not present, or unable to fulfil request
-        super
       end # class WatchmanFileScanner
     end # class FileScanner
   end # class Scanner