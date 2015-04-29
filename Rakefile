wincent/command-t

commit bc379eddf954b5c4834a5d5338c597921f50febe

Replace and/or with &&/||
As a matter of hygiene, `and` and `or` are best avoided in typical
conditional contexts (their lower precedence can catch the unwary by
surprise). About the only place I feel they are called for now are
exceedingly rare post-fix statements (eg. `do_something and raise 'foo'`
and the like).

So, replace them with with `&&` and `||`. In addition to providing a
friendlier precedence, these are actually more readable, despite their
superficial ugliness (it's easier for the brain to match symbols amidst
a larger expression than English words among a string of words).

Additionally, change one `while ... do` to `while ...` for consistency;
the `do` is permitted here but best reserved for block (lambda) syntax.

Rakefile
@@ -47,7 +47,7 @@ def prepare_release_notes
       break unless lines.shift == '' &&
                   (line = lines.shift) && line =~ /^\d\.\d/ &&
                   lines.shift == ''
-      while line = lines.shift and line != ''
+      while line = lines.shift && line != ''
         out.puts line
       end
       break

autoload/commandt.vim
@@ -162,7 +162,7 @@ ruby << EOF
     load_path_modified = false
     ::VIM::evaluate('&runtimepath').to_s.split(',').each do |path|
       lib = "#{path}/ruby"
-      if !$LOAD_PATH.include?(lib) and File.exist?(lib)
+      if !$LOAD_PATH.include?(lib) && File.exist?(lib)
         $LOAD_PATH << lib
         load_path_modified = true
       end

ruby/command-t/match_window.rb
@@ -197,7 +197,7 @@ module CommandT
     def find(char)
       # is this a new search or the continuation of a previous one?
       now = Time.now
-      if @last_key_time.nil? or @last_key_time < (now - 0.5)
+      if @last_key_time.nil? || @last_key_time < (now - 0.5)
         @find_string = char
       else
         @find_string += char

ruby/command-t/prompt.rb
@@ -150,7 +150,7 @@ module CommandT
       # prevent the status line from getting inadvertantly cleared
       # after our echo commands
       ::VIM::command 'redraw'
-      while (highlight = args.shift) and  (text = args.shift) do
+      while (highlight = args.shift) && (text = args.shift)
         text = VIM::escape_for_single_quotes text
         ::VIM::command "echohl #{highlight}"
         ::VIM::command "echon '#{text}'"

ruby/command-t/util.rb
@@ -77,7 +77,7 @@ module CommandT
           IO.popen(%w[/usr/sbin/pmcycles -m]).read.count("\n")
         elsif File.executable?('/usr/sbin/lsdev')
           IO.popen(%w[/usr/sbin/lsdev -Cc processor -S 1]).read.count("\n")
-        elsif File.executable?('/usr/sbin/sysconf') and os_name =~ /irix/i
+        elsif File.executable?('/usr/sbin/sysconf') && os_name =~ /irix/i
           IO.popen(%w[/usr/sbin/sysconf NPROC_ONLN]).read.to_i
         elsif File.executable?('/usr/sbin/sysctl')
           IO.popen(%w[/usr/sbin/sysctl -n hw.ncpu]).read.to_i

commit 2f2f5cea2f7ec22795178bfded1201cec84945a0

Fix release note preparation bug
I broke this in bc379eddf9. Ironically, at the time I said:

> As a matter of hygiene, `and` and `or` are best avoided in typical
> conditional contexts (their lower precedence can catch the unwary by
> surprise).

I still think that `and` can be surprising due to precedence, but here I
got stung by another arguable smell: using an assignment in a `while`
condition. It's a common pattern, and in C GCC will warn you unless you
write it as `((foo = thing))`, but in Ruby there's a strong idiomatic
tendency to avoid parens. I've met in the middle here by adding single
parens but not double ones.

Rakefile
@@ -113,7 +113,7 @@ task :notes do
       break unless lines.shift == '' &&
                   (line = lines.shift) && line =~ /^\d\.\d/ &&
                   lines.shift == ''
-      while line = lines.shift && line != ''
+      while (line = lines.shift) && line != ''
         out.puts line
       end
       break