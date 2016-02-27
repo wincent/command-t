53d9732ce88411a46abf8ff54784271e331b36b0
Greg Hurrell <greg@hurrell.net>
2016-02-26 18:57:50 -0800

    Remove mistaken memoization cleverness
    So, I realized that my thinking here was flawed and the math I'd done to
    show that this would work was flawed, and worked out magically just
    because I'd chosen a rectangle with very specific dimensions. Bad luck.
    Here's a counter example:
    
    - Consider needle length 5, haystack length 100. Observe the following
      collisions with the formula:
    
         needle_idx * needle_len + haystack_idx =     x
              0           0            80       =     80
              1           5            75       =     80
              2           10           70       =     80
              3           15           65       =     80
              4           20           60       =     80
    
    So with all those conditions you can expect a faster run because of all
    the (false) cache hits. But I think in practice it didn't matter much at
    all because the matrix is still sparse enough that hits were pretty
    rare. Either that, or I had a compensating bug somewhere else. Some
    support is leant to theory that collisions were rare, or the impact of
    them on scoring was relatively low, because I wasn't aware of any
    noticeable ranking bugs.
    
      Before [ie. 1f41e7630]  (best of three runs):
    
                                user     system      total        real
        pathological        2.920000   0.010000   2.930000 (  2.927735)
        command-t           1.850000   0.010000   1.860000 (  1.854239)
        chromium (subset)   2.260000   0.200000   2.460000 (  1.510716)
        chromium (whole)   11.090000   0.040000  11.130000 (  6.670843)
    
      After (best of three runs):
    
                                user     system      total        real
        pathological        2.170000   0.010000   2.180000 (  2.185652)
        command-t           1.750000   0.010000   1.760000 (  1.753948)
        chromium (subset)   2.270000   0.200000   2.470000 (  1.535449)
        chromium (whole)   10.940000   0.040000  10.980000 (  6.708795)
    
    Note that for the "before" test I went back to 1f41e7630 to get past the
    intermediate broken states and weirdness that I've had on this branch of
    late.

--- ruby/command-t/match.c
+++ ruby/command-t/match.c
@@ -8,275 +8,6 @@
 
 #define UNSET DBL_MAX
 
-/**
- * # Notes on memoization
- *
- * We want to store a matrix of `needle_len` rows and `haystack_len` columns so
- * that we can look up the previously computed values for "match of `needle_idx`
- * at `haystack_idx`".
- *
- * Assume `needle_len` and `haystack_len` are 5 and 10 respectively, our 2D grid
- * looks like the following. Given a `needle_idx` and a `haystack_idx`, we can
- * index into this co-ordinate space:
- *
- *         haystack_idx
- *      0 1 2 3 4 5 6 7 8 9
- *     +-------------------+
- *     | | | | | | | | | | | 0
- *     ---------------------
- *     | | | | | | | | | | | 1
- *     ---------------------
- *     | | | | | | | | | | | 2   needle_idx
- *     ---------------------
- *     | | | | | | | | | | | 2
- *     ---------------------
- *     | | | | | | | | | | | 3
- *     +-------------------+
- *
- *  Note, however, that we know there are some cells in the matrix that cannot
- *  possibly represent valid matches, because the needle couldn't entirely fit.
- *  Let's mark these with a `#`:
- *
- *         haystack_idx
- *      0 1 2 3 4 5 6 7 8 9
- *     +-------------------+
- *     | | | | | | |#|#|#|#| 0
- *     ---------------------
- *     |#| | | | | | |#|#|#| 1
- *     ---------------------
- *     |#|#| | | | | | |#|#| 2   needle_idx
- *     ---------------------
- *     |#|#|#| | | | | | |#| 2
- *     ---------------------
- *     |#|#|#|#| | | | | | | 3
- *     +-------------------+
- *
- * Observe that this wasted space is equal to `needle_len ^ 2 - needle_len` (a
- * valid assumption because `needle_len` is always smaller than or equal to
- * `haystack_len`, otherwise our search would short circuit before we even got
- * to the memoization stage). The reason the space relationship here is
- * quadratic can be intuited from the diagram, where you can see how the two
- * triangles in opposite corners could be used to form a square, were it not for
- * the missing slice of length `needle_len`.
- *
- * Also note that we don't actually store a real 2-dimensional array here but
- * rather a single array of size `needle_len * haystack_len`, meaning that a
- * small matrix like this one:
- *
- *      +-----+
- *      |A|B|C|
- *      -------
- *      |D|E|F|
- *      -------
- *      |G|H|I|
- *      +-----+
- *
- * Actually gets stored in memory as:
- *
- *      +-----------------+
- *      |A|B|C|D|E|F|G|H|I|
- *      +-----------------+
- *
- * The math we could use to go from a `haystack_idx`/`needle_idx` pair to an
- * index into this underlying 1-dimensional array could be something like:
- *
- *      const index = needle_idx * haystack_len + needle_idx;
- *
- * Or, if we wanted our table to be rotated by 90 degrees (with `needle_indx`
- * down the side):
- *
- *      const index = haystack_idx * needle_len + haystack_idx;
- *
- *  But note that in the code we're doing neither of those but instead:
- *
- *      const index = needle_idx * needle_len + haystack_idx;
- *
- *  This has a very interesting property of filling up the cells as follows:
- *
- *  - Assume cells contain values "A" through "Z" and then "a" through "z" (ie.
- *    the 30 valid values):
- *
- *    label needle haystack cell         label needle haystack cell
- *           idx     idx                        idx     idx
- *      A     0       0      0             P     0       5      5
- *      B     0       1      1             Q     1       5      10
- *      C     1       1      6             R     2       5      15
- *      D     0       2      2             S     3       5      20
- *      E     1       2      7             T     4       5      25
- *      F     2       2      12            U     1       6      11
- *      G     0       3      3             V     2       6      16
- *      H     1       3      8             W     3       6      21
- *      I     2       3      13            X     4       6      26
- *      J     3       3      1             Y     2       7      17
- *      K     0       4      4             Z     3       7      22
- *      L     1       4      9             a     4       7      27
- *      M     2       4      14            b     3       8      23
- *      N     3       4      19            c     4       8      28
- *      O     4       4      24            d     4       9      29
- *
- *      0 1 2 3 4 5 6 7 8 9
- *     +-------------------+
- *     |A|B|D|G|K|P|C|E|H|L| 0
- *     ---------------------
- *     |Q|U|F|I|M|R|V|Y|J|N| 1
- *     ---------------------
- *     |S|W|Z|b|O|T|X|a|c|d| 2
- *     ---------------------
- *     |#|#|#|#|#|#|#|#|#|#| 2
- *     ---------------------
- *     |#|#|#|#|#|#|#|#|#|#| 3
- *     +-------------------+
- *
- * Note that even the largest values for `needle_idx` and `haystack_idx` do not
- * place us anywhere in the bottom 2 rows, so we can actually avoid allocating
- * them entirely. When `needle_idx` is `needle_len - 1` (ie. the last character
- * in the needle) and `haystack_idx` is `haystack_len - 1` (ie. the last
- * character in the haystack) our index into the one-dimensional storage array
- * is `4 * 5 + 9`, which is 29, and corresponds to the cell containing "d" in
- * the diagram above.
- *
- *      0 1 2 3 4 5 6 7 8 9
- *     +-------------------+
- *     |A|B|D|G|K|P|C|E|H|L| 0
- *     ---------------------
- *     |Q|U|F|I|M|R|V|Y|J|N| 1
- *     ---------------------
- *     |S|W|Z|b|O|T|X|a|c|d| 2
- *     +-------------------+
- *
- * So, if we have a `needle_len` by `haystack_len` grid and corresponding set of
- * values (ie. 5 by 10, or 50 cells in this example), how is it that we are
- * writing to and reading from only a subset of 30 cells (3 rows of 10 cells
- * each) without any collisions? The answer lies in excluding the illegal
- * combinations where the needle cannot fit, which we mentioned above. With this
- * formula for cell assignment, the invalid cells all get packed into the bottom
- * two rows (marked with `#`). These correspond to the triangles in the corners
- * of the original diagrams above.
- *
- * As an additional optimization, note that we do a prescan which provides us
- * with information about the rightmost possible location for the final match of
- * the needle string with the haystack. This means that instead of using
- * `haystack_len` to figure out the range of positions at the end of the
- * haystack that some needle characters could not possibly occupy, we can use a
- * potentially lower `rightmost_match_p` value. For each additional unit that
- * `rightmost_match_p` is below `haystack_len - 1`, we can cross out another
- * section from our grid, meaning that we allocate even less memory:
- *
- *      0 1 2 3 4 5 6 7 8 9
- *     +-------------------+
- *     |A|B|D|G|C|E|H|K|F|I| 0
- *     ---------------------
- *     |L|O|J|M|P|R|N|Q|S|T| 1
- *     +-------------------+
- *
- * (Note that we haven't just eliminated the row here, we've also rearranged
- * the letters. I'll show how this was derived later on.)
- *
- * One way to visualize what's happening here is to go back to our original
- * diagram and number the cells as follows, based on the tabulation we made
- * earlier:
- *
- *         haystack_idx
- *      0 1 2 3 4 5 6 7 8 9
- *     +-------------------+
- *     |A|B|D|G|K|P|#|#|#|#| 0
- *     ---------------------
- *     |#|C|E|H|L|Q|U|#|#|#| 1
- *     ---------------------
- *     |#|#|F|I|M|R|V|Y|#|#| 2  needle_idx
- *     ---------------------
- *     |#|#|#|J|N|S|W|Z|b|#| 3
- *     ---------------------
- *     |#|#|#|#|O|T|X|a|c|d| 4
- *     +-------------------+
- *
- * In other words we fill in from top to bottom, and left to right, avoiding
- * cells which we know should be marked with a "#".
- *
- * Now imagine "chopping off" the corner triangles leaving only the rhomboidal
- * section which contains valid values:
- *
- *     +-----------+
- *     |A|B|D|G|K|P|
- *     ---------------
- *       |C|E|H|L|Q|U|
- *       ---------------
- *         |F|I|M|R|V|Y|
- *         ---------------
- *           |J|N|S|W|Z|b|
- *           ---------------
- *             |O|T|X|a|c|d|
- *             +-----------+
- *
- * And then "straightening" the shape back up into a rectangle using a shear
- * transformation:
- *
- *     +-----------+
- *     |A|B|D|G|K|P|
- *     -------------
- *     |C|E|H|L|Q|U|
- *     -------------
- *     |F|I|M|R|V|Y|
- *     -------------
- *     |J|N|S|W|Z|b|
- *     -------------
- *     |O|T|X|a|c|d|
- *     +-----------+
- *
- * And finally encoding the rectangle using a one-dimensional array:
- *
- *     +-----------------------------------------------------------+
- *     |A|B|D|G|K|P|C|E|H|L|Q|U|F|I|M|R|V|Y|J|N|S|W|Z|b|O|T|X|a|c|d|
- *     +-----------------------------------------------------------+
- *
- * If we additionally apply the `rightmost_match_p`-based trimming that we
- * discussed above, our intermediate states can be reduced to something like:
- *
- *         haystack_idx
- *      0 1 2 3 4 5 6 7 8 9
- *     +-------------------+
- *     |A|B|D|G|#|#|#|#|#|#| 0
- *     ---------------------
- *     |#|C|E|H|K|#|#|#|#|#| 1
- *     ---------------------
- *     |#|#|F|I|L|O|#|#|#|#| 2  needle_idx
- *     ---------------------
- *     |#|#|#|J|M|P|R|#|#|#| 3
- *     ---------------------
- *     |#|#|#|#|N|Q|S|T|#|#| 4
- *     +-------------------+
- *
- * Here our `rightmost_match_p` enforces a lower ceiling (by 2) beneath the one
- * provide by `haystack_len - 1`, which means that we have 10 more cells that
- * become invalid and are marked with "#". Moving on, we get:
- *
- *     +-------+
- *     |A|B|D|G|
- *     ---------
- *     |C|E|H|K|
- *     ---------
- *     |F|I|L|O|
- *     ---------
- *     |J|M|P|R|
- *     ---------
- *     |N|Q|S|T|
- *     +-------+
- *
- * And finally:
- *
- *     +---------------------------------------+
- *     |A|B|D|G|C|E|H|K|F|I|L|O|J|M|P|R|N|Q|S|T|
- *     +---------------------------------------+
- *
- * Which corresponds to the chart from earlier when I first introduced the
- * `rightmost_match_p` concept.
- *
- * These smaller arrays reduce memory usage and may be more likely to fit within
- * a CPU cache. Whether the non-linear access pattern during the execution of
- * the algorithm works well with the cache is an open question, although my
- * benchmarking suggests that the format is working well.
- */
-
 // Use a struct to make passing params during recursion easier.
 typedef struct {
     char    *haystack_p;            // Pointer to the path string to be searched.
@@ -302,14 +33,13 @@ double recursive_match(
     double score_for_char;
 
     // Do we have a memoized result we can return?
-    double *memoized = &m->memo[needle_idx * m->needle_len + haystack_idx];
+    double *memoized = &m->memo[haystack_idx * m->needle_len + needle_idx];
     if (*memoized != UNSET) {
         return *memoized;
     }
 
     // Iterate over needle.
     for (i = needle_idx; i < m->needle_len; i++) {
-
         // Iterate over (valid range of) haystack.
         for (j = haystack_idx + i; j <= m->rightmost_match_p[i]; j++) {
             char c = m->needle_p[i];
@@ -318,7 +48,7 @@ double recursive_match(
                 if (j == 0 || m->haystack_p[j - 1] == '/') { // This is a dot-file.
                     int dot_search = c == '.'; // Searching for a dot.
                     if (dot_search) {
-                        m->always_show_dot_files = 1;
+                        m->always_show_dot_files = 1; // Overwrite. Hack alert!
                     }
                     if (
                         m->never_show_dot_files ||
@@ -453,13 +183,8 @@ double calculate_match(
         }
 
         // Prepare for memoization.
-        // - Snip off corners.
-        // - Valid because we know needle_len < haystack_len from above.
-        // - Avoid collisions above with a guard clause.
         long haystack_limit = rightmost_match_p[m.needle_len - 1] + 1;
-        long memo_size =
-            haystack_limit * m.needle_len -
-            (m.needle_len * m.needle_len - m.needle_len);
+        long memo_size = m.needle_len * haystack_limit;
         double memo[memo_size];
         for (i = 0; i < memo_size; i++) {
             memo[i] = UNSET;
