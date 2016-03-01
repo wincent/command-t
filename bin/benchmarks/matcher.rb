3622026fe209c3341954561a74a94d910e53aee1
Greg Hurrell <greg@hurrell.net>
2016-03-01 09:23:51 -0800

    Show more stats for benchmark runs
    Not beautiful code, but just wanting to get a handle on the variability
    of results by running 10 times and showing standard deviation info.

--- bin/benchmarks/matcher.rb
+++ bin/benchmarks/matcher.rb
@@ -18,24 +18,93 @@ threads = CommandT::Util.processor_count
 
 puts "Starting benchmark run (PID: #{Process.pid})"
 
-Benchmark.bmbm do |b|
-  data['tests'].each do |test|
-    scanner = OpenStruct.new(:paths => test['paths'])
-    matcher = CommandT::Matcher.new(scanner)
-    b.report(test['name']) do
-      test['times'].times do
-        test['queries'].each do |query|
-          query.split(//).reduce('') do |acc, char|
-            query = acc + char
-            matcher.sorted_matches_for(
-              query,
-              :threads => threads,
-              :recurse => ENV.fetch('RECURSE', '1') == '1'
-            )
-            query
+# Run the benchmarks 10 times so that we can report some variance numbers too.
+results = 10.times.map do
+  Benchmark.bmbm do |b|
+    data['tests'].each do |test|
+      scanner = OpenStruct.new(:paths => test['paths'])
+      matcher = CommandT::Matcher.new(scanner)
+      b.report(test['name']) do
+        test['times'].times do
+          test['queries'].each do |query|
+            query.split(//).reduce('') do |acc, char|
+              query = acc + char
+              matcher.sorted_matches_for(
+                query,
+                :threads => threads,
+                :recurse => ENV.fetch('RECURSE', '1') == '1'
+              )
+              query
+            end
           end
         end
       end
     end
   end
+end
+
+# https://en.wikipedia.org/wiki/Winsorising
+def winsor(items)
+  length = items.length
+  return items if length < 5
+  items = items.sort
+  [items[1]] + items[1..length - 2] + [items[length - 2]]
+end
+
+results = results.reduce({}) do |acc, run|
+  run.each do |result|
+    acc[result.label] ||= {}
+    acc[result.label]['real'] ||= []
+    acc[result.label]['real'] << result.real
+    acc[result.label]['total'] ||= []
+    acc[result.label]['total'] << result.total
+  end
+  acc
+end
+
+results.keys.each do |label|
+  test = results[label]
+
+  test['real (best)'] = test['real'].min
+  test['total (best)'] = test['total'].min
+
+  test['real'] = winsor(test['real'])
+  test['total'] = winsor(test['total'])
+
+  test['real (avg)'] = test['real'].reduce(:+) / test['real'].length
+  test['total (avg)'] = test['total'].reduce(:+) / test['total'].length
+
+  test['real (variance)'] = test['real'].reduce(0) { |acc, value|
+    acc + (test['real (avg)'] - value) ** 2
+  } / test['real'].length
+  test['total (variance)'] = test['total'].reduce(0) { |acc, value|
+    acc + (test['total (avg)'] - value) ** 2
+  } / test['total'].length
+
+  test['real (sd)'] = Math.sqrt(test['real (variance)'])
+  test['total (sd)'] = Math.sqrt(test['total (variance)'])
+
+  test.delete('real')
+  test.delete('total')
+end
+
+puts "\n\nSummary:                                cpu time             (wall-clock time)\n"
+width = results.keys.map(&:length).max
+print ' ' * (width + 2)
+print '%9s ' % 'avg'
+print '%9s ' % 'best'
+print '%9s ' % 'sd'
+print '%9s ' % '(avg)'
+print '%9s ' % '(best)'
+print '%9s ' % '(sd)'
+puts
+results.each do |label, data|
+  print "%#{width}s " % label
+  print '   %.5f' % data['total (avg)']
+  print '   %.5f' % data['total (best)']
+  print '   %.5f' % data['total (sd)']
+  print ' (%.5f)' % data['real (avg)']
+  print ' (%.5f)' % data['real (best)']
+  print ' (%.5f)' % data['real (sd)']
+  puts
 end
