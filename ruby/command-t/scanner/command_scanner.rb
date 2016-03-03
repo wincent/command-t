d071d8b7073d6d70477a2a72848ffbb2e5b37d7d
Greg Hurrell <greg@hurrell.net>
2016-03-03 01:24:07 -0800

    Teach :CommandTCommand to find built-in Ex commands too
    Via a crude hack.

--- ruby/command-t/scanner/command_scanner.rb
+++ ruby/command-t/scanner/command_scanner.rb
@@ -11,11 +11,22 @@ module CommandT
     private
 
       def paths!
-        VIM.capture('silent command').split("\n")[2..-1].map do |line|
+        # Get user commands.
+        commands = VIM.capture('silent command').split("\n")[2..-1].map do |line|
           line.sub(/\A.{4}(\S+).+/, '\1')
         end
 
-        # TODO: merge with built-in commands (via rtp snooping)
+        # Get built-in commands from `ex-cmd-index`.
+        ex_cmd_index = ::VIM.evaluate('expand(findfile("doc/index.txt", &runtimepath))')
+        if File.readable?(ex_cmd_index)
+          File.readlines(ex_cmd_index).each do |line|
+            if line =~ %r{\A\|:([^|]+)\|\s+}
+              commands << $~[1]
+            end
+          end
+        end
+
+        commands.uniq
       end
     end # class CommandScanner
   end # class Scanner
