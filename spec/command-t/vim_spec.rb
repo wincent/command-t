3cbd857a4d508b628c7aea25656a20c46ab92ed6
Henric Trotzig <henric.trotzig@brigade.com>
2017-05-05 08:41:17 +0200

    Flesh out tests for `wildignore_to_regexp`
    To make this function more robust, and to make sure I understood it the
    right way, I'm adding some more tests. I've used the table in the docs
    [1] as a starting point, but I've only focused on the recommended list
    of patterns:
    
    > In short, in general you want to use patterns like:
      - `foo`: to match a file at any level.
      - `*.foo`: to match a file with an extension at any level.
      - `*/foo`: to match a file or directory at any level.
      - `*/foo/*`: to match a directory at any level.
    
    [1] https://github.com/wincent/command-t/blob/915048e6aed264dd7bf68e29ba485ade4c44f845/doc/command-t.txt#L1120-L1135

--- spec/command-t/vim_spec.rb
+++ spec/command-t/vim_spec.rb
@@ -13,10 +13,68 @@ describe CommandT::VIM do
   end
 
   describe '.wildignore_to_regexp' do
-    it 'properly matches directories at any level' do
-      regexp = Regexp.new(CommandT::VIM.wildignore_to_regexp('*/foo/*'))
-      expect(regexp).to match('some/path/to/foo/file.js')
-      expect(regexp).to_not match('some/path/to/foo')
+    subject do
+      Regexp.new(CommandT::VIM.wildignore_to_regexp(wildignore))
+    end
+
+    describe '"foo"' do
+      let(:wildignore) { 'foo' }
+
+      it 'matches the right strings' do
+        expect(subject).to_not match('a.foo')
+        expect(subject).to_not match('a/b.foo')
+        expect(subject).to match('foo')
+        expect(subject).to match('a/foo')
+        expect(subject).to_not match('a/foo/b')
+      end
+    end
+
+    describe '".foo"' do
+      let(:wildignore) { '*.foo' }
+
+      it 'matches the right strings' do
+        expect(subject).to match('a.foo')
+        expect(subject).to match('a/b.foo')
+        expect(subject).to_not match('foo')
+        expect(subject).to_not match('a/foo')
+        expect(subject).to_not match('a/foo/b')
+      end
+    end
+
+    describe '"*/foo/*"' do
+      let(:wildignore) { '*/foo' }
+
+      it 'matches the right strings' do
+        expect(subject).to_not match('a.foo')
+        expect(subject).to_not match('a/b.foo')
+        expect(subject).to match('foo')
+        expect(subject).to match('a/foo')
+        expect(subject).to match('a/foo/b')
+      end
+    end
+
+    describe '"*/foo/*"' do
+      let(:wildignore) { '*/foo/*' }
+
+      it 'matches the right strings' do
+        expect(subject).to_not match('a.foo')
+        expect(subject).to_not match('a/b.foo')
+        expect(subject).to_not match('foo')
+        expect(subject).to_not match('a/foo')
+        expect(subject).to match('a/foo/b')
+      end
+    end
+
+    describe 'multiple patterns' do
+      let(:wildignore) { '*.foo,*/foo/*' }
+
+      it 'matches the right strings' do
+        expect(subject).to match('a.foo')
+        expect(subject).to match('a/b.foo')
+        expect(subject).to_not match('foo')
+        expect(subject).to_not match('a/foo')
+        expect(subject).to match('a/foo/b')
+      end
     end
   end
 end
