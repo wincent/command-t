be96c6da38bfd70f86ed30f18e4b071367fd32cc
Kevin Cox <kevincox@google.com>
2017-06-24 11:32:39 +0100

    Add limit to matcher benchmark.
    Without a limit the time is dominated by sorting all the results in a
    single thread. The limit is configurable so it can be raised for testing
    merging.

--- bin/benchmarks/matcher.rb
+++ bin/benchmarks/matcher.rb
@@ -3,14 +3,7 @@
 # Copyright 2013-present Greg Hurrell. All rights reserved.
 # Licensed under the terms of the BSD 2-clause license.
 
-<<<<<<< HEAD
-%w[ext lib].each do |dir|
-  path  = File.expand_path("../../ruby/command-t/#{dir}", File.dirname(__FILE__))
-  $LOAD_PATH.unshift(path) unless $LOAD_PATH.include?(path)
-end
-=======
 require_relative 'base.rb'
->>>>>>> 5ec4b0b... Add C scanner infrastructure.
 
 require 'ostruct'
 require 'yaml'
@@ -33,11 +26,12 @@ class MatcherBenchmark < CommandT::Benchmark
           test['queries'].each do |query|
             query.split(//).reduce('') do |acc, char|
               query = acc + char
-              matcher.sorted_matches_for(
-                query,
-                :threads => @threads,
-                :recurse => ENV.fetch('RECURSE', '1') == '1'
-              )
+
+              matcher.sorted_matches_for query,
+                threads: @threads,
+                recurse: ENV.fetch('RECURSE', '1') == '1',
+                limit: ENV.fetch('LIMIT', '200').to_i
+
               query
             end
           end
