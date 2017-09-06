wincent/command-t

commit 85949ce7a70a26abf6f9d355e38d1473facba022

Fix unwanted split opening behavior
Similar to the fix we applied in 595976eb70ba3a2f7a2932eeb4 to fix
https://github.com/wincent/command-t/issues/295 looks like we have to
deal with this edge case too.

Closes https://github.com/wincent/command-t/issues/298

autoload/commandt.vim
@@ -165,7 +165,7 @@ function! commandt#GotoOrOpen(command_and_args) abort
   " instead of switching to the other tab; but bufexists() sees hidden
   " buffers, and if we try to open one of those, we get an unwanted split.
   if bufwinnr('^' . l:file . '$') != -1 ||
-        \ (bufexists(l:file) && !s:BufHidden(l:file))
+        \ (bufexists('^' . l:file . '$') && !s:BufHidden(l:file))
     execute 'sbuffer ' . l:file
   else
     execute l:command . l:file

commit 212b570239b42ff633976590ea858f10ea3087a2

Fix regression trying to switch to previously opened file in other tab
This is a regression introduced in 85949ce7.

That one attempted to fix #298 by applying a similar fix to the one used
in #295. However, it only worked by luck: `bufexists()` doesn't support
patterns or use of "$"/"^" to anchor the match like `bufwinnr()` does.
This meant that adding those anchors made it always false.

That fixed the unwanted splits in #298, but it meant that you couldn't
go back to an existing file in another tab.

The fix is pretty straightforward: switch from `bufexists()` to
`bufname()`, because the latter does support anchoring. As far as I can
tell, this fixes #306 without breaking the scenario detailed in #298.
The one gotcha here is that we can't rely on the truthiness of the
non-empty string returned by `bufname()` here, because crazy old
Vimscript coerces also strings to `0`; we therefore have to use an
explicit check against `''`.

Closes: https://github.com/wincent/command-t/issues/306

autoload/commandt.vim
@@ -161,11 +161,11 @@ function! commandt#GotoOrOpen(command_and_args) abort
   let l:command = l:command_and_args[0]
   let l:file = l:command_and_args[1]
 
-  " bufwinnr() doesn't see windows in other tabs, meaning we open them again
-  " instead of switching to the other tab; but bufexists() sees hidden
+  " `bufwinnr()` doesn't see windows in other tabs, meaning we open them again
+  " instead of switching to the other tab; but `bufname()` sees hidden
   " buffers, and if we try to open one of those, we get an unwanted split.
   if bufwinnr('^' . l:file . '$') != -1 ||
-        \ (bufexists('^' . l:file . '$') && !s:BufHidden(l:file))
+        \ (bufname('^' . l:file . '$') !=# '' && !s:BufHidden(l:file))
     execute 'sbuffer ' . l:file
   else
     execute l:command . l:file