348e12e9b94d91fd3e0cb8c76c642409308ab5d8
Greg Hurrell <wincent@github.com>
2022-07-08 18:35:12 +0200

    chore(lua): copy watchman files
    I'm going to strip the Rubyisms out of these, but committing them as-is
    first (these are direct copies of the `ruby/` files, made with `cp`), so
    that I can see the diff in subsequent commit(s).

---
+++ lua/wincent/commandt/watchman.h
@@ -0,0 +1,34 @@
+/**
+ * SPDX-FileCopyrightText: Copyright 2014-present Greg Hurrell and contributors.
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <ruby.h>
+
+/**
+ * @module CommandT::Watchman::Utils
+ *
+ * Methods for working with the Watchman binary protocol
+ *
+ * @see https://github.com/facebook/watchman/blob/master/website/_docs/BSER.markdown
+ */
+
+/**
+ * Convert an object serialized using the Watchman binary protocol[0] into an
+ * unpacked Ruby object
+ */
+extern VALUE CommandTWatchmanUtils_load(VALUE self, VALUE serialized);
+
+/**
+ * Serialize a Ruby object into the Watchman binary protocol format
+ */
+extern VALUE CommandTWatchmanUtils_dump(VALUE self, VALUE serializable);
+
+/**
+ * Issue `query` to the Watchman instance listening on `socket` (a `UNIXSocket`
+ * instance) and return the result
+ *
+ * The query is serialized following the Watchman binary protocol and the
+ * result is converted to native Ruby objects before returning to the caller.
+ */
+extern VALUE CommandTWatchmanUtils_query(VALUE self, VALUE query, VALUE socket);
