578f3023ddcbb933bc3b4d6876bcca60c7264f0e
Greg Hurrell <greg@hurrell.net>
2022-07-11 00:48:29 +0200

    perf(lua): add first cut at benchmarks
    Example Ruby output:
    
        Starting benchmark run (PID: 39554)
        Rehearsal -----------------------------------------------------
        pathological        0.842283   0.044546   0.886829 (  0.887976)
        command-t           0.718605   0.014775   0.733380 (  0.734401)
        chromium (subset)   2.889448   0.375507   3.264955 (  0.584594)
        chromium (whole)    3.794513   0.036333   3.830846 (  0.542142)
        big (400k)          5.748676   0.062536   5.811212 (  0.819703)
        ------------------------------------------- total: 14.527222sec
    
                                user     system      total        real
        pathological        0.788505   0.006421   0.794926 (  0.796025)
        command-t           0.668498   0.009170   0.677668 (  0.678368)
        chromium (subset)   2.889850   0.386840   3.276690 (  0.595200)
        chromium (whole)    3.807340   0.033035   3.840375 (  0.560542)
        big (400k)          5.876608   0.038175   5.914783 (  0.823454)
    
    Lua output:
    
        Rehearsal------------      total         wall
        pathological            1.265143   (1.266931)
        command-t               0.286175   (0.286615)
        chromium (subset)       0.453491   (0.163662)
        chromium (whole)        0.518156   (0.135855)
        big (400k)              0.825949   (0.215629)
    
        Final----------------      total         wall
        pathological            1.266634   (1.268609)
        command-t               0.301985   (0.323223)
        chromium (subset)       0.456851   (0.165168)
        chromium (whole)        0.522492   (0.137989)
        big (400k)              0.823315   (0.214049)
    
    So, yeah, there may be a few suspicious things about these numbers:
    
    1. "pathological" numbers seem too high (both cpu and wall).
    
    2. Other numbers seem too low.
    
    3. I am, as noted in the comments, in a sense comparing "apples and
       oranges" because I don't think the Ruby tests pass a `limit` option.
       Having said that, if I _do_ pass one in, it doesn't seem to have a very
       big impact on the tests, which makes me wonder if I am even threading
       it through in all the right places... But I added logging and confirmed
       that without the limit, I get 15 results at most (ie. the default
       limit), and with a high limit (like 10,000), I get more... (eg. in the
       "command-t" tests I get 109 results for one of the queries). Who
       knows? Maybe LuaJIT is just insanely fast, or I am somehow
       "cheating" without knowing how. Or maybe I introduced a big bug in my
       porting... We'll see over time, I guess.
    
    On the bright side, the difference between cpu and wall clock times does
    appear to indicate that at least threading is working as expected. The
    smaller tests see no difference because they are below the necessary
    threshold for threading to kick in. But the bigger stests all show the
    expected roughly 4x improvement.

--- bin/benchmarks/matcher.lua
+++ bin/benchmarks/matcher.lua
@@ -3,41 +3,70 @@
 -- SPDX-FileCopyrightText: Copyright 2014-present Greg Hurrell and contributors.
 -- SPDX-License-Identifier: BSD-2-Clause
 
+local ffi = require'ffi'
+
 local pwd = os.getenv('PWD')
 local benchmarks_directory  = debug.getinfo(1).source:match('@?(.*/)')
 local lua = pwd .. '/' .. benchmarks_directory .. '../../lua/'
 
 package.path = lua .. '?.lua;' .. package.path
 package.path = lua .. '?/init.lua;' .. package.path
+package.path = pwd .. '/' .. benchmarks_directory .. '../../data/?.lua;' .. package.path
 
 -- Note, may need to modify package.cpath too.
 local commandt = require'wincent.commandt'
 
--- TODO want to print something like
---  $user-cpu-time $system-cpu-time ($wall-clock-time) -- rehearsal
---  $user-cpu-time $system-cpu-time ($wall-clock-time) -- actual
+-- Using a Lua module for benchmark data so that we don't need to pull in a JSON
+-- or YAML dependency.
+local data = require'wincent.benchmark'
 
+local lib = require'wincent.commandt.lib'
 
 commandt.epoch() -- Force eager loading of C library.
 
-local start_cpu = os.clock()
-local start_wall_s, start_wall_us = commandt.epoch()
+local options = {
+  -- TODO may want to put something in here (like a high limit) to make this an
+  -- apples-to-apples comparison
+  -- although in reality, no client will (or should) ever ask for more than,
+  -- say, 100 matches...
+}
 
-local s = 0
-for i = 1, 100000 do
-  s = s + i
-end
+for i = 1, tonumber(os.getenv('TIMES') or 20) do
+  for _, mode in ipairs({'Rehearsal', 'Final----'}) do
+    print('\n' .. mode .. '------------      total         wall')
 
-local end_cpu = os.clock()
-local end_wall_s, end_wall_us = commandt.epoch()
+    for _, config in ipairs(data.tests) do
+      local scanner = lib.scanner_new_copy(config.paths)
+      local matcher = lib.commandt_matcher_new(scanner, options)
 
-local wall_delta = (function ()
-  if end_wall_us >= start_wall_s then
-    end_wall_us = end_wall_us + 1000000
-    end_wall_s = end_wall_s - 1
+      local start_cpu = os.clock()
+      local start_wall_s, start_wall_us = commandt.epoch()
+
+      for j = 1, config.times do
+        for _, query in ipairs(config.queries) do
+          local input = ''
+          for letter in query:gmatch('.') do
+            input = input .. letter
+            local results = lib.commandt_matcher_run(matcher, query)
+            for k = 0, results.count - 1 do
+              local str = results.matches[k]
+              ffi.string(str.contents, str.length) -- Neovim would do something here...
+            end
+          end
+        end
+      end
+
+      local end_cpu = os.clock()
+      local end_wall_s, end_wall_us = commandt.epoch()
+      local wall_delta = (function ()
+        if end_wall_us >= start_wall_s then
+          end_wall_us = end_wall_us + 1000000
+          end_wall_s = end_wall_s - 1
+        end
+        return (end_wall_s - start_wall_s) + (end_wall_us - start_wall_us) / 1000000
+      end)()
+
+      print(string.format('%-22s  %.6f   (%.6f)', config.name, end_cpu - start_cpu, wall_delta))
+    end
   end
-  return (end_wall_s - start_wall_s) + (end_wall_us - start_wall_us) / 1000000
-end)()
-
-print(string.format("elapsed (CPU): %.6f", end_cpu - start_cpu))
-print(string.format("elapsed (wall): %.6f", wall_delta))
+end
