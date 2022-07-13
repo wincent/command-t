faf6011517d89301913a9e1fa8fb00ff885d47cd
Greg Hurrell <greg@hurrell.net>
2022-07-14 00:55:25 +0200

    feat(lua): add historical comparison to benchmarking
    Lots of code here, so it might have bugs (I'll check it tomorrow), but
    this prints out stuff like this:
    
        Summary of cpu time and (wall time):
    
                            best    avg      sd     +/-      p    (best)  (avg)    (sd)     +/-      p
        chromium (subset) 2.81499 4.07109 1.72634 [+4.1%] 0.0005 0.52112 0.85807 0.61572 [+14.8%] 0.0005
               big (400k) 5.38775 6.75959 1.92037 [+1.9%] 0.0005 0.74014 1.08010 0.64347 [+13.4%] 0.0005
         chromium (whole) 3.31954 4.36914 1.42175 [+1.5%]  0.005 0.45263 0.72852 0.51081 [+18.8%] 0.0005
             pathological 0.45680 0.70984 0.35737 [+5.3%] 0.0005 0.45725 0.71781 0.37912  [+6.3%] 0.0005
                command-t 0.41544 0.64982 0.33837 [+5.1%] 0.0005 0.41565 0.65967 0.37454  [+6.3%] 0.0005
    
    based on data logged to `data/wincent/benchmark/log.lua`.
    
    It's hot here, so the above is the result of running twice in a row with
    `TIMES=10`, and we see a real (as indicated by the p values) slow down
    due to thermal throttling. If I let the machine cool down for a minute
    and then run again, I see this:
    
        Summary of cpu time and (wall time):
    
                            best    avg      sd      +/-      p    (best)  (avg)    (sd)     +/-      p
        chromium (subset) 2.82297 3.00507 0.31863 [-35.5%] 0.0005 0.54307 0.58483 0.07685 [-46.7%] 0.0005
               big (400k) 4.98921 5.39459 0.63969 [-25.3%] 0.0005 0.76641 0.85499 0.30155 [-26.3%] 0.0005
         chromium (whole) 3.25751 3.55554 0.45382 [-22.9%] 0.0005 0.47375 0.53356 0.10181 [-36.5%] 0.0005
             pathological 0.45641 0.47504 0.05603 [-49.4%] 0.0005 0.45725 0.48064 0.09039 [-49.3%] 0.0005
                command-t 0.42371 0.43316 0.02380 [-50.0%] 0.0005 0.42432 0.43432 0.02498 [-51.9%] 0.0005
    
    Of course, with the hot weather, it's going to be really hard to get
    sensible comparisons for a while (it's 40 degrees celcius, 100+ degrees
    farenheit). In order to stay cool we have to run only a small number of
    times, but in order to get high signal data, we have to run lots of
    times... so...
    
    Still to do, as noted in the comment, sort the table so it shows the
    rows in the same order as we use elsewhere.

--- bin/benchmarks/matcher.lua
+++ bin/benchmarks/matcher.lua
@@ -15,9 +15,11 @@ package.path = pwd .. '/' .. benchmarks_directory .. '../../data/?.lua;' .. pack
 
 local commandt = require'wincent.commandt'
 
--- Using a Lua module for benchmark data so that we don't need to pull in a JSON
+-- We use Lua modules for benchmark data so that we don't need to pull in a JSON
 -- or YAML dependency.
 local data = require'wincent.benchmark'
+local ok, log = pcall(require, 'wincent.benchmark.log')
+log = ok and log or {}
 
 local lib = require'wincent.commandt.lib'
 
@@ -34,8 +36,14 @@ local options = {
   -- none in Ruby one
 }
 
+local results = {
+  when = os.date(),
+  timings = {},
+}
+
 for i = 1, tonumber(os.getenv('TIMES') or 20) do
-  for _, mode in ipairs({'Rehearsal', 'Final----'}) do
+  for _, rehearsal in ipairs({true, false}) do
+    local mode = rehearsal and 'Rehearsal' or 'Final----'
     print('\n' .. mode .. '------------      total         wall')
 
     for _, config in ipairs(data.tests) do
@@ -49,16 +57,16 @@ for i = 1, tonumber(os.getenv('TIMES') or 20) do
         for _, query in ipairs(config.queries) do
           local input = ''
           for letter in query:gmatch('.') do
-            local results = lib.commandt_matcher_run(matcher, input)
-            for k = 0, results.count - 1 do
-              local str = results.matches[k]
+            local matches = lib.commandt_matcher_run(matcher, input)
+            for k = 0, matches.count - 1 do
+              local str = matches.matches[k]
               ffi.string(str.contents, str.length)
             end
             input = input .. letter
           end
-          local results = lib.commandt_matcher_run(matcher, input)
-          for k = 0, results.count - 1 do
-            local str = results.matches[k]
+          local matches = lib.commandt_matcher_run(matcher, input)
+          for k = 0, matches.count - 1 do
+            local str = matches.matches[k]
             ffi.string(str.contents, str.length)
           end
         end
@@ -66,6 +74,8 @@ for i = 1, tonumber(os.getenv('TIMES') or 20) do
 
       local end_cpu = os.clock()
       local end_wall_s, end_wall_us = commandt.epoch()
+
+      local cpu_delta = end_cpu - start_cpu
       local wall_delta = (function()
         if end_wall_us >= start_wall_s then
           end_wall_us = end_wall_us + 1000000
@@ -74,7 +84,316 @@ for i = 1, tonumber(os.getenv('TIMES') or 20) do
         return (end_wall_s - start_wall_s) + (end_wall_us - start_wall_us) / 1000000
       end)()
 
-      print(string.format('%-22s  %.6f   (%.6f)', config.name, end_cpu - start_cpu, wall_delta))
+      print(string.format('%-22s  %.6f   (%.6f)', config.name, cpu_delta, wall_delta))
+
+      if not rehearsal then
+        results.timings[config.name] = results.timings[config.name] or {
+          cpu = {},
+          wall = {},
+        }
+        table.insert(results.timings[config.name].cpu, cpu_delta)
+        table.insert(results.timings[config.name].wall, wall_delta)
+      end
     end
   end
 end
+
+local dump
+
+local avg = function(values)
+  local sum = 0
+  for _, value in ipairs(values) do
+    sum = sum + value
+  end
+  return sum / #values
+end
+
+local DIFFERENCE = 1
+local ABSOLUTE_DIFFERENCE = 2
+local SIGNEDNESS = 3
+local SIGNED_RANK = 4
+
+-- Test for significance via Wilcoxon Signed Rank test.
+--
+-- See: http://vassarstats.net/textbook/ch12a.html
+local significance = function(last, current)
+  if #last ~= #current then
+    return 0
+  end
+
+  local zipped = {}
+  for i, l in ipairs(last) do
+    local difference = l - current[i]
+    if difference ~= 0 then
+      local absolute_difference = math.abs(difference)
+      local signedness = difference / absolute_difference -- 1 or -1.
+      table.insert(zipped, {difference, absolute_difference, signedness})
+    end
+  end
+
+  table.sort(zipped, function(a, b)
+    return a[ABSOLUTE_DIFFERENCE] < b[ABSOLUTE_DIFFERENCE]
+  end)
+
+  local rank = 1
+  local values = {}
+  local ranked = {}
+
+  for i, row in ipairs(zipped) do
+    local count = 0
+    for j, inner_row in ipairs(zipped) do
+      if inner_row[ABSOLUTE_DIFFERENCE] == row[ABSOLUTE_DIFFERENCE] then
+        count = count + 1
+        table.insert(values, j)
+      end
+    end
+    local acc = 0
+    for _, value in ipairs(values) do
+      acc = acc + value
+    end
+    rank = acc / count
+    table.insert(ranked, {
+      row[DIFFERENCE],
+      row[ABSOLUTE_DIFFERENCE],
+      row[SIGNEDNESS],
+      row[SIGNEDNESS] * rank
+    })
+  end
+
+  local n = #ranked
+  local w = 0
+
+  for _, row in ipairs(ranked) do
+    w = w + row[SIGNED_RANK]
+  end
+
+  local p_value = 0
+  if n < 10 then
+    local thresholds = ({
+      {},
+      {},
+      {},
+      {},
+      {},
+      {{15, 0.05}},
+      {{17, 0.05}, {21, 0.025}},
+      {{22, 0.05}, {25, 0.025}, {28, 0.01}},
+      {{26, 0.05}, {30, 0.025}, {34, 0.01}, {36, 0.005}},
+      {{29, 0.05}, {35, 0.025}, {39, 0.01}, {43, 0.005}},
+    })[n + 1]
+    while true do
+      local limit = table.remove(thresholds, #thresholds)
+      if limit == nil then
+        break
+      end
+      if math.abs(w) >= limit[1] then
+        p_value = limit[2]
+        break
+      end
+    end
+  else
+    local sd = math.sqrt(n * (n + 1) * (2 * n + 1) / 6)
+    local z = math.abs((w - 0.5) / sd)
+    if z >= 3.291 then
+      p_value = 0.0005
+    elseif z >= 2.576 then
+      p_value = 0.005
+    elseif z >= 2.326 then
+      p_value = 0.01
+    elseif z >= 1.960 then
+      p_value = 0.025
+    elseif z >= 1.645 then
+      p_value = 0.05
+    end
+  end
+
+  return p_value
+end
+
+local variance = function(values)
+  local mean = avg(values)
+  local result = 0
+  for _, value in ipairs(values) do
+    result = result + (mean - value) ^ 2
+  end
+  return result
+end
+
+local previous = log[#log]
+
+for label, metrics in pairs(results.timings) do
+  metrics['cpu (best)'] = math.min(unpack(metrics.cpu))
+  metrics['wall (best)'] = math.min(unpack(metrics.wall))
+
+  metrics['cpu (avg)'] = avg(metrics.cpu)
+  metrics['wall (avg)'] = avg(metrics.wall)
+
+  local cpu_avg = metrics['cpu (avg)']
+  local wall_avg = metrics['wall (avg)']
+
+  if previous then
+    local previous_cpu_avg = previous.timings[label]['cpu (avg)']
+    local previous_cpu = previous.timings[label].cpu
+    local previous_wall_avg = previous.timings[label]['wall (avg)']
+    local previous_wall = previous.timings[label].wall
+
+    metrics['cpu (+/-)'] = (cpu_avg - previous_cpu_avg) / cpu_avg * 100
+    metrics['wall (+/-)'] = (wall_avg - previous_wall_avg) / wall_avg * 100
+
+    metrics['cpu (significance)'] = significance(previous_cpu, metrics.cpu)
+    metrics['wall (significance)'] = significance(previous_wall, metrics.wall)
+
+  else
+    metrics['cpu (+/-)'] = 0
+    metrics['wall (+/-)'] = 0
+
+    metrics['cpu (significance)'] = 0
+    metrics['wall (significance)'] = 0
+  end
+
+  metrics['cpu (variance)'] = variance(metrics.cpu)
+  metrics['wall (variance)'] = variance(metrics.wall)
+
+  metrics['cpu (sd)'] = math.sqrt(metrics['cpu (variance)'])
+  metrics['wall (sd)'] = math.sqrt(metrics['wall (variance)'])
+end
+
+dump = function(value, indent)
+  indent = indent or ''
+  if type(value) == 'nil' then
+    return 'nil'
+  elseif type(value) == 'number' then
+    return tostring(value)
+  elseif type(value) == 'string' then
+    if value:match("'") then
+      error('dump(): cannot serialize string containing single quote')
+    end
+    return "'" .. value .. "'"
+  elseif type(value) == 'table' then
+    if value[1] ~= nil then -- Assume it's a list.
+      local output = '{\n'
+      for _, item in ipairs(value) do
+        output = output .. indent .. '  ' .. dump(item, indent .. '  ') .. ',\n'
+      end
+      return output .. indent .. '}'
+    else
+      local output = '{\n'
+      for key, item in pairs(value) do
+        output = output .. indent .. '  ' ..
+          '[' .. dump(key) .. '] = ' ..
+          dump(item, indent .. '  ') .. ',\n'
+      end
+      return output .. indent .. '}'
+    end
+  else
+    error('dump(): unsupported type ' .. type(value))
+  end
+end
+
+table.insert(log, results)
+local file = io.open(pwd .. '/' .. benchmarks_directory .. '../../data/wincent/benchmark/log.lua', 'w')
+file:write('-- @generated\nreturn ' .. dump(log) .. '\n')
+file:close()
+
+local center = function(text)
+  return {
+    align = 'center',
+    text = text,
+  }
+end
+
+local float = function(number)
+  return string.format('%.5f', number)
+end
+
+-- Remove trailing zeros.
+local trim = function(number)
+  return tostring(number):gsub('0+$', '')
+end
+
+print('\n\nSummary of cpu time and (wall time):\n')
+
+local summary = {{
+  '',
+  center('best'),
+  center('avg'),
+  center('sd'),
+  center('+/-'),
+  center('p'),
+  center('(best)'),
+  center('(avg)'),
+  center('(sd)'),
+  center('+/-'),
+  center('p'),
+}}
+
+for label, data in pairs(results.timings) do
+  table.insert(summary, {
+    label,
+    float(data['cpu (best)']),
+    float(data['cpu (avg)']),
+    float(data['cpu (sd)']),
+    string.format('[%+0.1f%%]', data['cpu (+/-)']),
+    data['cpu (significance)'] > 0 and trim(data['cpu (significance)']) or '',
+    float(data['wall (best)']),
+    float(data['wall (avg)']),
+    float(data['wall (sd)']),
+    string.format('[%+0.1f%%]', data['wall (+/-)']),
+    data['wall (significance)'] > 0 and trim(data['wall (significance)']) or '',
+  })
+end
+
+local round = function(number)
+  return math.floor(number + 0.5)
+end
+
+local align = function(stringish, width)
+  if type(stringish) == 'string' then
+    return string.format('%' .. width .. 's', stringish)
+  elseif stringish.align == 'center' then
+    local padding = round((width - #(stringish.text)) / 2)
+    return string.format(
+      '%' .. padding .. 's%s%' .. padding .. 's',
+      '',
+      stringish.text,
+      ''
+    ):sub(1, width)
+  elseif stringish.align == 'left' then
+    return string.format('%-' .. width .. 's', stringish.text)
+  else
+    return string.format('%' .. width .. 's', stringish.text)
+  end
+end
+
+local reduce = function(list, initial, cb)
+  local acc = initial
+  for i, value in ipairs(list) do
+    acc = cb(acc, value, i)
+  end
+  return acc
+end
+
+local print_table = function(rows)
+  for _, row in ipairs(rows) do
+    local output = ''
+    for i, cell in ipairs(row) do
+      local width = reduce(rows, 0, function(acc, value)
+        local length = type(value[i]) == 'string' and #(value[i]) or #(value[i].text)
+        if length > acc then
+          return length
+        else
+          return acc
+        end
+      end)
+      if i == 1 then
+        output = output .. align(cell, width)
+      else
+        output = output .. ' ' .. align(cell, width)
+      end
+    end
+    print(output)
+  end
+end
+
+-- TODO: sort rows to use same order as benchmark input data
+print_table(summary)
