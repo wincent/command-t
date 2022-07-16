17d166edfe86701c621e9c6223e5b14faca6cadd
Greg Hurrell <wincent@github.com>
2022-07-16 18:39:38 +0200

    feat: add check-format.sh
    And rename prettier.sh to format.sh to match.

---
+++ bin/check-format.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: Copyright 2022-present Greg Hurrell and contributors.
+# SPDX-License-Identifier: BSD-2-Clause
+
+REPO_ROOT="${BASH_SOURCE%/*}/.."
+
+# Must `cd` to root in order for `stylua.toml` to get picked up.
+cd "$REPO_ROOT"
+
+stylua --check .
