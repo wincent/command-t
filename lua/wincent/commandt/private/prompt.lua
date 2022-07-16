33e9c952e93cac8d99394465ae15d8bb41bda042
Greg Hurrell <greg@hurrell.net>
2022-07-17 00:03:31 +0200

    refactor(lua): patch in Window to display prompt
    Window isn't fully general yet, but it will become more so as I use it
    for the match listing too, in a future commit.

--- lua/wincent/commandt/private/prompt.lua
+++ lua/wincent/commandt/private/prompt.lua
@@ -3,134 +3,26 @@
 
 local prompt = {}
 
-local window = require('wincent.commandt.private.window')
+local Window = require('wincent.commandt.private.window').Window
 
-local input_buffer = nil
-local input_window = nil
-local title_buffer = nil
-local title_window = nil
+local window = nil
 
 prompt.show = function(options)
   -- TODO: merge options
-  if input_buffer == nil then
-    input_buffer = vim.api.nvim_create_buf(
-      false, -- listed
-      true -- scratch
-    )
-    if input_buffer == 0 then
-      error('wincent.commandt.private.prompt.show(): nvim_create_buf() failed')
-    end
-    local ps1 = '> '
-    vim.api.nvim_buf_set_option(input_buffer, 'buftype', 'prompt')
-    vim.fn.prompt_setprompt(input_buffer, ps1)
-    vim.api.nvim_buf_set_option(input_buffer, 'filetype', 'CommandTPrompt')
-    vim.api.nvim_create_autocmd('TextChanged', {
-      buffer = input_buffer,
-      callback = function()
+  if window == nil then
+    window = Window.new({
+      buftype = 'prompt',
+      filetype = 'CommandTPrompt',
+      onchange = function(contents)
         if options and options.onchange then
-          -- Should be able to use `prompt_getprompt()`, but it only returns the
-          -- prompt prefix for some reason...
-          -- local query = vim.fn.prompt_getprompt(input_buffer)
-          local query = vim.api.nvim_get_current_line():sub(#ps1 + 1)
-          options.onchange(query)
+          options.onchange(contents)
         end
       end,
-    })
-    vim.api.nvim_create_autocmd('TextChangedI', {
-      buffer = input_buffer,
-      callback = function()
-        if options and options.onchange then
-          local query = vim.api.nvim_get_current_line():sub(#ps1 + 1)
-          options.onchange(query)
-        end
-      end,
+      title = 'CommandT [type]', -- TODO make real
     })
   end
-  if input_window == nil then
-    local width = vim.o.columns
-    input_window = vim.api.nvim_open_win(
-      input_buffer,
-      true, -- enter
-      {
-        border = 'single',
-        col = 0,
-        focusable = false,
-        height = 1,
-        noautocmd = true,
-        relative = 'editor',
-        row = vim.o.lines - 3,
-        style = 'minimal',
-        width = width,
-      }
-    )
-    if input_window == 0 then
-      error('wincent.commandt.private.prompt.show(): nvim_open_win() failed')
-    end
-    -- TODO: maybe watch for buffer destruction too
-    -- TODO: watch for resize events
-    vim.api.nvim_create_autocmd('WinClosed', {
-      once = true,
-      callback = function()
-        input_window = nil
-      end,
-    })
-    vim.api.nvim_win_set_option(input_window, 'wrap', false)
-  end
-  vim.api.nvim_buf_set_lines(
-    input_buffer,
-    0, -- start
-    -1, -- end
-    false, -- strict indexing
-    {} -- replacement lines
-  )
-  -- This is verbose; make utility methods...
-  if title_buffer == nil then
-    title_buffer = vim.api.nvim_create_buf(
-      false, -- listed
-      true -- scratch
-    )
-    if title_buffer == 0 then
-      error('wincent.commandt.private.prompt.show(): nvim_create_buf() failed')
-    end
-    vim.api.nvim_buf_set_option(title_buffer, 'buftype', 'nofile')
-    vim.api.nvim_buf_set_option(title_buffer, 'filetype', 'CommandTTitle')
-  end
-  local prompt_title = ' Command-T [type] '
-  if title_window == nil then
-    title_window = vim.api.nvim_open_win(
-      title_buffer,
-      false, -- enter
-      {
-        col = 3,
-        focusable = false,
-        height = 1,
-        noautocmd = true,
-        relative = 'editor',
-        row = vim.o.lines - 4,
-        style = 'minimal',
-        width = #prompt_title,
-        win = prompt_window,
-        zindex = 60, -- Default for floats is 50
-      }
-    )
-    if title_window == 0 then
-      error('wincent.commandt.private.prompt.show(): nvim_open_win() failed')
-    end
-  end
-  vim.api.nvim_buf_set_lines(
-    title_buffer,
-    0, -- start
-    -1, -- end
-    false, -- strict indexing
-    { prompt_title } -- TODO: put actual type
-  )
-  vim.api.nvim_set_current_win(input_window)
-  vim.cmd('startinsert')
-  -- vim.api.nvim_feedkeys(
-  --   'i', -- keys
-  --   'n', -- don't remap
-  --   true -- escape K_SPECIAL bytes in keys
-  -- )
+  window:show()
+  window:focus()
 end
 
 return prompt
--- lua/wincent/commandt/private/window.lua
+++ lua/wincent/commandt/private/window.lua
@@ -8,6 +8,8 @@
 
 local window = {}
 
+local merge = require('wincent.commandt.private.merge')
+
 local Window = {}
 
 local mt = {
@@ -22,6 +24,8 @@ function Window.new(options)
     filetype = nil,
     --onclose = nil,
     onchange = nil,
+    prompt = '> ',
+    title = 'Command-T',
   }, options)
   local w = {
     _buftype = options.buftype,
@@ -31,6 +35,8 @@ function Window.new(options)
     _main_window = nil,
     _onchange = options.onchange,
     _position = options.position,
+    _prompt = options.prompt,
+    _title = options.title,
     _title_buffer = nil,
     _title_window = nil,
   }
@@ -40,21 +46,123 @@ end
 
 -- Focus the window and enter insert mode, ready to receive input.
 function Window:focus()
-  -- TODO: if not shown, show first, then...
-  vim.api.nvim_set_current_win(self.main_window)
+  -- TODO: if not shown, show first automatically?, then...
+  vim.api.nvim_set_current_win(self._main_window)
   vim.cmd('startinsert')
 end
 
 function Window:show()
-  if self.main_buffer == nil then
+  if self._main_buffer == nil then
+    self._main_buffer = vim.api.nvim_create_buf(
+      false, -- listed = false
+      true -- scratch = true
+    )
+    if self._main_buffer == 0 then
+      error('Window:show(): nvim_create_buf() failed')
+    end
+    local ps1 = self._prompt or '> '
+    if self._buftype == 'prompt' then
+      vim.api.nvim_buf_set_option(self._main_buffer, 'buftype', 'prompt')
+      vim.fn.prompt_setprompt(self._main_buffer, ps1)
+    end
+    if self._filetype ~= nil then
+      vim.api.nvim_buf_set_option(self._main_buffer, 'filetype', self._filetype)
+    end
+    if self._onchange then
+      local callback = function()
+        -- Should be able to use `vim.fn.prompt_getprompt(self._main_buffer)`,
+        -- but it only returns the prompt prefix for some reason...
+        local query = vim.api.nvim_get_current_line():sub(#ps1 + 1)
+        self._onchange(query)
+      end
+      vim.api.nvim_create_autocmd('TextChanged', {
+        buffer = self._main_buffer,
+        callback = callback,
+      })
+      vim.api.nvim_create_autocmd('TextChangedI', {
+        buffer = self._main_buffer,
+        callback = callback,
+      })
+    end
   end
-  if self.main_window == nil then
+  if self._main_window == nil then
+    local width = vim.o.columns
+    self._main_window = vim.api.nvim_open_win(
+      self._main_buffer,
+      true, -- enter = true
+      {
+        border = 'single',
+        col = 0,
+        focusable = false,
+        height = 1,
+        noautocmd = true,
+        relative = 'editor',
+        row = vim.o.lines - 3,
+        style = 'minimal',
+        width = width,
+      }
+    )
+    if self._main_window == 0 then
+      error('Window:show(): nvim_open_win() failed')
+    end
+    -- TODO: maybe watch for buffer destruction too
+    -- TODO: watch for resize events
+    vim.api.nvim_create_autocmd('WinClosed', {
+      once = true,
+      callback = function()
+        self._main_window = nil
+      end,
+    })
+    vim.api.nvim_win_set_option(self._main_window, 'wrap', false)
+    -- TODO: decide whether I need to clear lines here.
+    vim.api.nvim_buf_set_lines(
+      self._main_buffer,
+      0, -- start
+      -1, -- end
+      false, -- strict indexing = false
+      {} -- replacement lines
+    )
   end
-  if self.title_buffer == nil then
+  if self._title_buffer == nil then
+    self._title_buffer = vim.api.nvim_create_buf(
+      false, -- listed = false
+      true -- scratch = true
+    )
+    if self._title_buffer == 0 then
+      error('Window:show(): nvim_create_buf() failed')
+    end
+    vim.api.nvim_buf_set_option(self._title_buffer, 'buftype', 'nofile')
+    vim.api.nvim_buf_set_option(self._title_buffer, 'filetype', 'CommandTTitle')
   end
-  if self.title_window == nil then
+  local prompt_title = ' ' .. self._title .. ' '
+  if self._title_window == nil then
+    self._title_window = vim.api.nvim_open_win(
+      self._title_buffer,
+      false, -- enter = false
+      {
+        col = 3,
+        focusable = false,
+        height = 1,
+        noautocmd = true,
+        relative = 'editor',
+        row = vim.o.lines - 4,
+        style = 'minimal',
+        width = #prompt_title,
+        zindex = 60, -- Default for floats is 50
+      }
+    )
+    if self._title_window == 0 then
+      error('Window:show(): nvim_open_win() failed')
+    end
   end
-  -- TODO: position title
+  -- TODO: position title autoatically
+  vim.api.nvim_buf_set_lines(
+    self._title_buffer,
+    0, -- start
+    -1, -- end
+    false, -- strict indexing
+    { prompt_title } -- TODO: put actual type
+  )
 end
 
 window.Window = Window
