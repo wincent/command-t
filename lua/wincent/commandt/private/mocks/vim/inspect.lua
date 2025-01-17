2df38e0c3669b7f89761f574021c1ae4cd94fbd5
Greg Hurrell <greg@hurrell.net>
2025-01-17 20:45:06 +0100

    test: add missing module
    The old "forgot to run `git-add`" trick.

---
+++ lua/wincent/commandt/private/mocks/vim/inspect.lua
@@ -0,0 +1,16 @@
+-- SPDX-FileCopyrightText: Copyright 2025-present Greg Hurrell and contributors.
+-- SPDX-License-Identifier: BSD-2-Clause
+
+local M = {}
+
+function M.setup()
+  if _G.vim == nil then
+    _G.vim = {}
+  end
+
+  _G.vim.inspect = function(value)
+    return '<inspect:' .. type(value) .. '>'
+  end
+end
+
+return M
