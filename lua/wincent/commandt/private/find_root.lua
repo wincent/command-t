1a4801ae794fcd4ed26d53fead52f28fa5fd617b
Greg Hurrell <greg.hurrell@datadoghq.com>
2025-06-13 12:15:00 +0200

    feat: improve `find_root`
    Add a safety valve.
    
    Don't rely on broken `simplify()`.

--- lua/wincent/commandt/private/find_root.lua
+++ lua/wincent/commandt/private/find_root.lua
@@ -4,27 +4,27 @@
 local find_root = nil
 
 find_root = function(starting_directory, root_markers)
-  local absolute_path = vim.fn.fnamemodify(starting_directory, ':p')
+  -- Make absolute.
+  starting_directory = vim.fn.fnamemodify(starting_directory, ':p')
 
   -- If it's a directory, :p will add a trailing slash, which we must strip.
-  absolute_path = absolute_path:gsub('(.-)/$', '%1')
+  starting_directory = starting_directory:gsub('(.-)/$', '%1')
 
-  while true do
+  local next_path = starting_directory
+  local attempts = 0
+  while attempts < 100 do
+    attempts = attempts + 1
     for _, marker in ipairs(root_markers) do
-      local candidate = absolute_path .. '/' .. marker
+      local candidate = next_path .. '/' .. marker
       if vim.fn.isdirectory(candidate) == 1 or vim.fn.filereadable(candidate) == 1 then
-        return absolute_path
+        return next_path
       end
     end
-    local next_path = vim.fn.simplify(absolute_path .. '/..')
-    if string.find(absolute_path, '^/+$') then
-      -- The `string.find()` is to make sure we bail if passed a bad path with
-      -- multiple leading separators (eg. '////', which is the same as '/' but
-      -- doesn't look like it).
-      return vim.fn.getcwd()
-    end
-    absolute_path = next_path
+    next_path = vim.fs.normalize(vim.fs.joinpath(next_path, '..'))
   end
+
+  -- Found nothing.
+  return starting_directory
 end
 
 return find_root
