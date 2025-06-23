wincent/command-t

commit 08f4ce135ab7abff4252ac465f0559f8748f01a2

fix: use C11 atomic functions for concurrent counter updates
There is a small race here between the main thread and the worker
threads (to update the `matches_count` counter).

Best to be on the safe side and use C11 atomics to do the counter
updates. Note that I could have stayed in POSIX land and used full-blown
mutexes, but this feels lighter weight for my use case.

Benchmarks show no negative effect (and nor would I expect one; there
simply aren't enough threads):

    Summary of cpu time and (wall time):

                        best    avg      sd     +/-      p     (best)    (avg)      (sd)     +/-     p
         pathological 0.20625 0.21953 0.09259 [-3.1%]   0.05 (0.20651) (0.21983) (0.09266) [-2.9%]
            command-t 0.16559 0.17099 0.04981 [-4.9%]  0.005 (0.16579) (0.17123) (0.04985) [-4.8%]  0.01
    chromium (subset) 1.34399 1.35204 0.02009 [+1.5%] 0.0005 (0.28246) (0.28741) (0.01198) [+0.2%]
     chromium (whole) 1.10621 1.10930 0.01033 [-1.0%] 0.0005 (0.12126) (0.12457) (0.01005) [-2.3%]  0.01
           big (400k) 1.66403 1.66947 0.02844 [-0.8%] 0.0005 (0.18023) (0.18398) (0.01161) [-1.2%]  0.05
                total 4.49569 4.52134 0.12853 [-0.4%]  0.025 (0.96018) (0.98702) (0.12068) [-2.0%] 0.005

lua/wincent/commandt/lib/matcher.c
@@ -7,6 +7,7 @@
 
 #include <assert.h> /* for assert */
 #include <pthread.h> /* for pthread_create, pthread_join etc */
+#include <stdatomic.h> /* for atomic_fetch_add(), atomic_load() */
 #include <stdbool.h> /* for bool */
 #include <stddef.h> /* for size_t */
 #include <stdlib.h> /* for qsort(), NULL */
@@ -100,7 +101,7 @@ result_t *commandt_matcher_run(matcher_t *matcher, const char *needle) {
     scanner_t *scanner = matcher->scanner;
     unsigned candidate_count = scanner->count;
     unsigned limit = matcher->limit;
-    unsigned matches_count = 0;
+    atomic_uint matches_count = 0;
 
     size_t needle_length = strlen(needle);
     char *needle_copy = xmalloc(needle_length + 1);
@@ -192,11 +193,11 @@ result_t *commandt_matcher_run(matcher_t *matcher, const char *needle) {
             // For the last worker, we'll just use the main thread.
             heap_t *heap = get_matches(&worker_args[i]);
             memcpy(
-                matches + matches_count,
+                matches + atomic_load(&matches_count),
                 heap->entries,
                 heap->count * sizeof(haystack_t *)
             );
-            matches_count += heap->count;
+            atomic_fetch_add(&matches_count, heap->count);
             heap_free(heap);
         } else {
             int err = pthread_create(
@@ -215,26 +216,29 @@ result_t *commandt_matcher_run(matcher_t *matcher, const char *needle) {
             die("phtread_join() failed", err);
         }
         memcpy(
-            matches + matches_count,
+            matches + atomic_load(&matches_count),
             heap->entries,
             heap->count * sizeof(haystack_t *)
         );
-        matches_count += heap->count;
+        atomic_fetch_add(&matches_count, heap->count);
         heap_free(heap);
     }
 
     free(threads);
     free(worker_args);
 
+    unsigned count = atomic_load(&matches_count);
     if (needle_length == 0 || (needle_length == 1 && matcher->needle[0] == '.')) {
         // Alphabetic order if search string is only "" or "."
-        qsort(matches, matches_count, sizeof(haystack_t *), cmp_alpha_p);
+        qsort(matches, count, sizeof(haystack_t *), cmp_alpha_p);
     } else {
-        qsort(matches, matches_count, sizeof(haystack_t *), cmp_score_p);
+        qsort(matches, count, sizeof(haystack_t *), cmp_score_p);
     }
 
+    if (count > limit) {
+        count = limit;
+    }
     result_t *results = xmalloc(sizeof(result_t));
-    unsigned count = matches_count > limit ? limit : matches_count;
     results->matches = xmalloc(count * sizeof(const char *));
     results->match_count = 0;
     results->candidate_count = candidate_count;

commit 41ca3dc2a87109d2bc6ac9f26aa18694ad36404e

fix: fix bug when `height` is set to a large value
There was a latent bug that I wasn't seeing with `height` 30 but I was
seeing with `height` 1000. I did some bisecting, and found the magic
number where the bug started to manifest more obviously was around a
height of 326. At that height, the bug would show up as zero files
coming back, or 4, or 20, or 31. Closing and reopening in succession
would produce different results, suggesting a racey ordering issue.

Let's see if I can explain it.

- A `height` of 326 ends up being passed in as a `limit` in the
  matcher. In the directory where I was seeing the bug, I had about
  3K files, and 10 CPU cores (and therefore 10 worker threads).
- So, each worker thread would end up getting as many as 326 files,
  for a total of 3,260 in all (or speaking with more precision, each
  worker would have a heap of size 326, but because there are only 3K
  files in the repo, the last worker would actually end up with an empty
  heap and the second-to-last one would only have a semi-full one).
- Because `count` (3,260) is greater than `limit` (326), we would re-set
  count to `326`, and allocate a `result_t` struct with storage for 326
  candidate strings.
- Then in the `for` loop below we would loop over the first 326 entries
  and copy any `candidate` with a non-zero score.

The obvious bug, then, is that you might end up skipping over a bunch of
results because they have zero scores, and in the worst case, the first
326 results you look at would all have zero scores and you would copy
nothing!

Bug was introduced in 08f4ce135ab7abff ("fix: use C11 atomic functions
for concurrent counter updates", 2024-08-17), so that makes that the
second dumb thing I've discovered so far that I did in that commit 🤦:

```diff
     free(threads);
     free(worker_args);

+    unsigned count = atomic_load(&matches_count);
     if (needle_length == 0 || (needle_length == 1 && matcher->needle[0] == '.')) {

lua/wincent/commandt/lib/matcher.c
@@ -229,11 +229,8 @@ result_t *commandt_matcher_run(matcher_t *matcher, const char *needle) {
         qsort(matches, count, sizeof(haystack_t *), cmp_score_p);
     }
 
-    if (count > limit) {
-        count = limit;
-    }
     result_t *results = xmalloc(sizeof(result_t));
-    results->matches = xmalloc(count * sizeof(const char *));
+    results->matches = xmalloc(limit * sizeof(const char *));
     results->match_count = 0;
     results->candidate_count = candidate_count;
 