51272332ccdadcbdd28a52c03602f59ad9cfb1ee
Greg Hurrell <greg.hurrell@datadoghq.com>
2025-07-02 16:44:53 +0200

    refactor: use `vim.fs.root()` instead of hand-rolled check

--- lua/wincent/commandt/private/find_root.lua
+++ lua/wincent/commandt/private/find_root.lua
@@ -11,27 +11,7 @@
 --- @param root_markers string[]
 --- @return string
 local function find_root(starting_directory, root_markers)
-  -- Make absolute.
-  starting_directory = vim.fn.fnamemodify(starting_directory, ':p')
-
-  -- If it's a directory, :p will add a trailing slash, which we must strip.
-  starting_directory = starting_directory:gsub('(.-)/$', '%1')
-
-  local next_path = starting_directory
-  local attempts = 0
-  while attempts < 100 do
-    attempts = attempts + 1
-    for _, marker in ipairs(root_markers) do
-      local candidate = next_path .. '/' .. marker
-      if vim.fn.isdirectory(candidate) == 1 or vim.fn.filereadable(candidate) == 1 then
-        return next_path
-      end
-    end
-    next_path = vim.fs.normalize(vim.fs.joinpath(next_path, '..'))
-  end
-
-  -- Found nothing.
-  return starting_directory
+  return vim.fs.root(starting_directory, root_markers) or starting_directory
 end
 
 return find_root
